1ccac59668b10e9921838491f38ebaf2c5e8ae96
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-11 14:32:02 +0100

    feat: add not found and error middlewares

--- server/app.js
+++ server/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const Sperror = require('sperror');
+
+app.use((req, res, next) => {
+  next(new Sperror('Not Found', "Couldn't found requested resource.", 404));
+});
+
+app.use((err, req, res, next) => {
+  err instanceof Sperror
+    ? res.status(err.statusCode).json({ error: err })
+    : res.status(500).json({
+        error: new Sperror(
+          'Server error',
+          'The server encountered an unexpected error.',
+          500
+        ),
+      });
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App listening on PORT ${process.env.PORT}`)
