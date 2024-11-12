62553003392a4e6f8eebfeaa79ccac3759c106c9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-12 17:05:12 +0100

    feat: add passport config functions

---
+++ server/helpers/passport/config.js
@@ -0,0 +1,20 @@
+const passport = require('passport');
+require('./github');
+require('./local');
+require('./guest');
+const prisma = require('../../models/queries');
+
+passport.serializeUser((user, done) => {
+  return user.id ? done(null, { id: user.id }) : done(null, { guest: true });
+});
+
+passport.deserializeUser(async (user, done) => {
+  try {
+    if (user.guest) return done(null, user);
+    const existingUser = await prisma.findUserById(user.id);
+    if (!existingUser) return done(null, false);
+    return done(null, { id: existingUser.id });
+  } catch (err) {
+    return done(err);
+  }
+});
