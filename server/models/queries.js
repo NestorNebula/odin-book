7567129e5bda032ea84bc88971bee7423044954b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-12 14:52:01 +0100

    feat: add findOrCreateUser query and categories

--- server/models/queries.js
+++ server/models/queries.js
@@ -2,4 +2,37 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-module.exports = {};
+// User
+
+const findOrCreateUser = async (username, email) => {
+  const user = await prisma.user.upsert({
+    where: {
+      email_loginMethod: {
+        email,
+        loginMethod: 'GITHUB',
+      },
+    },
+    update: {},
+    create: {
+      username,
+      email,
+      loginMethod: 'GITHUB',
+    },
+    select: { id: true },
+  });
+  return user;
+};
+
+// Profile
+
+// Post
+
+// Interaction
+
+// Message
+
+// Chat
+
+// Notification
+
+module.exports = { findOrCreateUser };
