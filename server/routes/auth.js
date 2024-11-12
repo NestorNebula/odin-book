ad9ce0747802520430637e54cfab87f98db6919e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-12 15:25:10 +0100

    feat: add routes for GitHub signin

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 const controller = require('../controllers/controller').auth;
+const passport = require('passport');
 
 router.post('/signup', controller.signUp);
+router.get(
+  '/signin/github',
+  passport.authenticate('github', { scope: ['user:email'], failWithError })
+);
+router.get(
+  '/signin/github/callback',
+  passport.authenticate('github', {
+    successRedirect: process.env.ORIGIN,
+    failWithError,
+  })
+);
 router.get('/logout', controller.logOut);
 
 module.exports = router;
