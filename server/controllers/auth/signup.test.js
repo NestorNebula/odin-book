bf4f80a5d32ba0b74cbe227ca4af389fd4b7cd35
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-13 11:20:22 +0100

    test: add tests for signup controller

---
+++ server/controllers/auth/signup.test.js
@@ -0,0 +1,47 @@
+const { request, app, data } = require('../../tests/setup');
+const router = require('../../routes/auth');
+
+app.use('/', router);
+
+describe('sign up', () => {
+  it('returns user after successful signup', () => {
+    return request(app)
+      .post('/signup')
+      .send({
+        username: 'username',
+        email: 'email@email.com',
+        password: 'securepassword',
+      })
+      .type('form')
+      .then((res) => {
+        expect(res.body.user.username).toBe('username');
+        expect(res.body.user.email).toBe('email@email.com');
+      });
+  });
+
+  it('returns 400 and errors when trying to create an account with data already taken', () => {
+    return request(app)
+      .post('/signup')
+      .send({ username: data.users[0].username, email: data.users[0].email })
+      .type('form')
+      .expect(400)
+      .then((res) => {
+        expect(res.body.errors).not.toBeNull();
+        expect(res.body.errors[0].msg).toMatch(/username already taken/i);
+        expect(res.body.errors[1].msg).toMatch(/email already taken/i);
+      });
+  });
+
+  it('returns 400 and errors when submitting incorrect data', () => {
+    return request(app)
+      .post('/signup')
+      .send({ username: 'averylongusername', email: 'incorrectemail' })
+      .type('form')
+      .expect(400)
+      .then((res) => {
+        expect(res.body.errors).not.ToBeNull();
+        expect(res.body.errors[0].msg).toMatch(/15 characters/i);
+        expect(res.body.errors[1].msg).toMatch(/email format/i);
+      });
+  });
+});
