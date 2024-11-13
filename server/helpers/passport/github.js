fc259d259cbd30d2f3041bf1ca2fabff04de637f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-13 14:39:38 +0100

    feat: add username check on github signin
    The username wasn't checked before creating account
    This was causing potential errors on account creation

--- server/helpers/passport/github.js
+++ server/helpers/passport/github.js
@@ -2,6 +2,7 @@ const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
 require('dotenv').config();
 const prisma = require('../../models/queries');
+const crypto = require('node:crypto');
 
 passport.use(
   new GitHubStrategy(
@@ -11,8 +12,19 @@ passport.use(
       callbackURL: process.env.CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
+      let username =
+        profile.username.length > 15
+          ? profile.username.substring(0, 15).toLowerCase()
+          : profile.username.toLowerCase();
+      let existingUser = await prisma.findUserByUsername(username);
+      let count = 0;
+      while (existingUser && count < 100) {
+        username = crypto.randomBytes(7).toString('hex');
+        existingUser = await prisma.findUserByUsername(username);
+        count++;
+      }
       const user = await prisma.findOrCreateUser(
-        profile.username,
+        username.toLowerCase(),
         profile.email
       );
       return done(null, user);
