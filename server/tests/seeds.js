3c5151c423d8edc6cf4f8f07c5397e914538b0c6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-13 11:17:15 +0100

    feat: add data, populateDb & emptyDb
    This will be used by tests setup

---
+++ server/tests/seeds.js
@@ -0,0 +1,47 @@
+const { faker } = require('@faker-js/faker');
+const prisma = require('../models/queries');
+const bcrypt = require('bcrypt');
+
+const getFakeUser = () => {
+  const username = faker.person.firstName().toLowerCase();
+  return {
+    username,
+    email: faker.internet.email({ firstName: username }).toLowerCase(),
+  };
+};
+
+const getFakeLocalUser = () => {
+  const user = getFakeUser();
+  user.password = faker.internet.password({ length: 20, memorable: true });
+  return user;
+};
+
+const getFakeGitHubUser = () => {
+  const user = getFakeUser();
+  user.password = null;
+  return user;
+};
+
+const data = {
+  users: [getFakeLocalUser(), getFakeGitHubUser(), getFakeGitHubUser()],
+};
+
+const populateDb = async (data) => {
+  data.users.forEach(async (user) => {
+    if (user.password) {
+      await prisma.createUser(
+        user.username,
+        user.email,
+        bcrypt.hashSync(user.password, 10)
+      );
+    } else {
+      await prisma.findOrCreateUser(user.username, user.email);
+    }
+  });
+};
+
+const emptyDb = async () => {
+  await prisma.deleteUsers();
+};
+
+module.exports = { data, populateDb, emptyDb };
