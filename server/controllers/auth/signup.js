5588bf2df38a941bba58205de4eae22ec9005bfa
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-13 15:40:01 +0100

    feat: add signup controller

--- server/controllers/auth/signup.js
+++ server/controllers/auth/signup.js
@@ -1,3 +1,28 @@
-const signUp = () => {};
+const prisma = require('../../models/queries');
+const { validateUser } = require('../../helpers/validation');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const Sperror = require('sperror');
+
+const signUp = [
+  validateUser,
+  async (req, res, next) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    const user = await prisma.createUser(
+      req.body.username,
+      req.body.email,
+      bcrypt.hashSync(req.body.password, 10)
+    );
+    if (!user) {
+      return next(
+        new Sperror('Server error', 'Error when creating user.', 500)
+      );
+    }
+    return res.json({ user });
+  },
+];
 
 module.exports = signUp;
