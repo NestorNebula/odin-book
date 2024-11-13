b4c1d819dad343a6a1b5e05738333b289096623d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-13 18:30:10 +0100

    feat: add getAll controller

--- server/controllers/user/getAll.js
+++ server/controllers/user/getAll.js
@@ -1,3 +1,23 @@
-const getAllUsers = () => {};
+const prisma = require('../../models/queries');
+
+const getAllUsers = async (req, res) => {
+  let users = [];
+  if (req.query.omit) {
+    users = await prisma.findNonFollowedUsers(
+      req.user.id,
+      !isNaN(+req.query.limit) ? +req.query.limit : 100
+    );
+  } else if (req.query.search) {
+    users = await prisma.findUsersBySearch(
+      req.query.search,
+      !isNaN(+req.query.limit) ? +req.query.limit : 100
+    );
+  } else {
+    users = await prisma.findUsers(
+      !isNaN(+req.query.limit) ? +req.query.limit : 100
+    );
+  }
+  res.json({ users });
+};
 
 module.exports = getAllUsers;
