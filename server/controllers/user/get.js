e42b81e993d55cc1a3e65a1268a78ef6b54dcc68
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-14 16:06:50 +0100

    feat: add getUser controller

--- server/controllers/user/get.js
+++ server/controllers/user/get.js
@@ -1,3 +1,12 @@
-const getUser = () => {};
+const prisma = require('../../models/queries');
+const Sperror = require('sperror');
+
+const getUser = async (req, res, next) => {
+  const user = await prisma.findUserById(+req.params.userId);
+  if (!user) {
+    return next(new Sperror('Not Found', "This user doesn't exist.", 404));
+  }
+  res.json({ user });
+};
 
 module.exports = getUser;
