daa8a1de2725cd80fc65c38a02ce23de2cab4537
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-14 18:13:33 +0100

    fix: avoid givin full user as userId param

--- server/controllers/user/put.test.js
+++ server/controllers/user/put.test.js
@@ -8,8 +8,9 @@ beforeAll(() => {
 
 describe('putUser', () => {
   it('returns user after successful update', async () => {
+    const user = await getReqUser(data);
     return request(app)
-      .put(`/${await getReqUser(data)}`)
+      .put(`/${user.id}`)
       .send({ username: 'newusername' })
       .type('form')
       .then((res) => {
@@ -18,8 +19,9 @@ describe('putUser', () => {
   });
 
   it('returns 400 and error when submitting incorrect data', async () => {
+    const user = await getReqUser(data);
     return request(app)
-      .put(`/${await getReqUser(data)}`)
+      .put(`/${user.id}`)
       .send({ username: 'new' })
       .type('form')
       .expect(400)
@@ -29,8 +31,9 @@ describe('putUser', () => {
   });
 
   it('returns 400 and error when github user try to update his email', async () => {
+    const user = await getReqUser(data);
     return request(app)
-      .put(`/${await getReqUser(data)}`)
+      .put(`/${user.id}`)
       .send({ email: 'mynew@email.com' })
       .type('form')
       .expect(400)
@@ -40,8 +43,9 @@ describe('putUser', () => {
   });
 
   it('returns 403 when trying to update another user', async () => {
+    const user = await getReqUser(data);
     return request(app)
-      .put(`/${(await getReqUser(data)) + 1}`)
+      .put(`/${user.id + 1}`)
       .send({ username: 'new' })
       .type('form')
       .expect(403);
