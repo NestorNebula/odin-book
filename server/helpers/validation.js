6cb27b21eaa3acb7fe34d57459ef78a5f616f22d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-14 17:54:19 +0100

    feat: add userUpdate validationChain

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -29,4 +29,38 @@ const validateUser = [
     .withMessage('Password must have at least 8 characters.'),
 ];
 
-module.exports = { validateUser };
+const validateUserUpdate = [
+  body('username')
+    .trim()
+    .escape()
+    .blacklist('<>@')
+    .toLowerCase()
+    .isLength({ min: 4, max: 15 })
+    .withMessage('Username must have between 4 and 15 characters.')
+    .custom(async (username) => {
+      if (!username) return;
+      const existingUser = await prisma.findUserByUsername(username);
+      if (existingUser) throw new Error('Username already taken.');
+    }),
+  body('email')
+    .trim()
+    .blacklist('<>')
+    .isEmail()
+    .withMessage("Email isn't a valid email.")
+    .normalizeEmail()
+    .custom(async (email, { req }) => {
+      if (!email) return;
+      const user = await prisma.findUserById(req.user.id);
+      if (user.loginMethod === 'GITHUB') {
+        throw new Error("Users logged with GitHub can't update their email.");
+      }
+      const existingUser = await prisma.findUserByUsermail(email);
+      if (existingUser) throw new Error('Email already taken.');
+    }),
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must have at least 8 characters.'),
+];
+
+module.exports = { validateUser, validateUserUpdate };
