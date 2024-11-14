2a22c969e81119609b8d2f35f30e9bc72f5138b9
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-14 08:22:40 +0100

    test: add tests for getUser

---
+++ server/controllers/user/get.test.js
@@ -0,0 +1,29 @@
+const { request, app, data } = require('../../tests/setup');
+const router = require('../../routes/user');
+const prisma = require('../../models/queries');
+
+let user = null;
+beforeAll(async () => {
+  user = await prisma.__findFullUserByUsername(data.users[0].username);
+});
+app.use((req, res, next) => {
+  req.user = user;
+  next();
+});
+app.use('/', router);
+
+describe('getUser', () => {
+  it('returns existing user', () => {
+    return request(app)
+      .get(`/${user.id}`)
+      .then((res) => {
+        expect(res.body.user.username).toBe(user.username);
+      });
+  });
+
+  it('returns 404 when trying to get a non-existing user', (done) => {
+    request(app)
+      .get(`/${user.id - 1}`)
+      .expect(404);
+  });
+});
