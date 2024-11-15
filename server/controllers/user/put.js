49d04c31af20bdd9c10af66b8896a4cc1234598d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-15 08:23:33 +0100

    feat: add putUser controller

--- server/controllers/user/put.js
+++ server/controllers/user/put.js
@@ -1,3 +1,31 @@
-const putUser = () => {};
+const Sperror = require('sperror');
+const { validationResult } = require('express-validator');
+const { validateUserUpdate } = require('../../helpers/validation');
+const bcrypt = require('bcrypt');
+const prisma = require('../../models/queries');
+
+const putUser = [
+  validateUserUpdate,
+  async (req, res, next) => {
+    if (req.user.id !== +req.params.userId) {
+      return next(new Sperror('Forbidden', "You can't update this data.", 403));
+    }
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    const user = await prisma.findUserById(req.user.id);
+    const password = req.body.password
+      ? bcrypt.hashSync(req.body.password, 10)
+      : user.password;
+    const updatedUser = await prisma.updateUser(
+      user.id,
+      req.body.username || user.username,
+      req.body.email || user.email,
+      password
+    );
+    res.json({ user: updatedUser });
+  },
+];
 
 module.exports = putUser;
