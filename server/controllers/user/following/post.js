e911f50759491690c3666c9ff0e39ef30a3ee482
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-15 11:10:55 +0100

    feat: add postFollowing controller

--- server/controllers/user/following/post.js
+++ server/controllers/user/following/post.js
@@ -1,3 +1,21 @@
-const postFollowing = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const postFollowing = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(new Sperror('Forbidden', "You can't update this data.", 403));
+  }
+  const userToFollow = await prisma.findUserById(+req.body.userId);
+  if (!userToFollow) {
+    return next(
+      new Sperror('Not Found', "Couldn't find the user to follow", 404)
+    );
+  }
+  const updatedUser = await prisma.connectUserFollowing(
+    req.user.id,
+    userToFollow.id
+  );
+  res.json({ user: updatedUser });
+};
 
 module.exports = postFollowing;
