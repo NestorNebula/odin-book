2d7512b476abfb14a8fbf6dffaeb82c6ee483c9a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-15 10:51:57 +0100

    test: add tests for postFollowing

---
+++ server/controllers/user/following/post.test.js
@@ -0,0 +1,36 @@
+const { request, app, data } = require('../../../tests/setup');
+const router = require('../../../routes/user');
+const { getReqUser } = require('../../../tests/reqUser');
+
+beforeAll(() => {
+  app.use('/', router);
+});
+
+describe('postFollowing', () => {
+  it('returns user with following after successful follow', async () => {
+    const user = await getReqUser(data);
+    return request(app)
+      .post(`/${user.id}/following`)
+      .send({ userId: user.id + 1 })
+      .expect(200)
+      .then((res) => {
+        expect(res.body.user.following.length).toBe(1);
+      });
+  });
+
+  it("returns 403 when trying to update someone else's follows", async () => {
+    const user = await getReqUser(data);
+    return request(app)
+      .post(`/${user.id + 1}/following`)
+      .send({ userId: user.id })
+      .expect(403);
+  });
+
+  it('returns 404 when trying to follow non-existent user', async () => {
+    const user = await getReqUser(data);
+    return request(app)
+      .post(`/${user.id}/following`)
+      .send({ userId: user.id - 1 })
+      .expect(404);
+  });
+});
