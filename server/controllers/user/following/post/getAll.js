eff69cb6fdfd6de2b0176ce6c0346ea623f65908
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-15 15:29:51 +0100

    feat: add getAllFollowingPosts controller

--- server/controllers/user/following/post/getAll.js
+++ server/controllers/user/following/post/getAll.js
@@ -1,3 +1,12 @@
-const getAllFollowingPosts = () => {};
+const prisma = require('../../../../models/queries');
+const Sperror = require('sperror');
+
+const getAllFollowingPosts = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(new Sperror('Forbidden', "You can't access this data.", 403));
+  }
+  const posts = await prisma.findUserFollowingPosts(req.user.id);
+  res.json({ posts });
+};
 
 module.exports = getAllFollowingPosts;
