2fbe45f8ebc2b3864f5c611fe16010db902ad3d2
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-15 12:24:57 +0100

    feat: add deleteFollowing controller

--- server/controllers/user/following/delete.js
+++ server/controllers/user/following/delete.js
@@ -1,3 +1,18 @@
-const deleteFollowing = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const deleteFollowing = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(new Sperror('Forbidden', "You can't update this data.", 403));
+  }
+  if (isNaN(+req.body.userId)) {
+    return next(new Sperror('Bad Request', 'No user provided.', 400));
+  }
+  const updatedUser = await prisma.disconnectUserFollowing(
+    req.user.id,
+    +req.body.userId
+  );
+  res.json({ user: updatedUser });
+};
 
 module.exports = deleteFollowing;
