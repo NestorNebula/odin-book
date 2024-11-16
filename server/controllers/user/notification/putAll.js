546c04bd6a8aa6e8fd8b5415baa142ee07377c2b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 10:36:15 +0100

    feat: add putAllNotifications controller

--- server/controllers/user/notification/putAll.js
+++ server/controllers/user/notification/putAll.js
@@ -1,3 +1,13 @@
-const putAllNotifications = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const putAllNotifications = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(new Sperror('Forbidden', "You can't update this data.", 403));
+  }
+  await prisma.updateNotifications(req.user.id);
+  const updatedNotifications = await prisma.findNotifications(req.user.id);
+  res.json({ notifications: updatedNotifications });
+};
 
 module.exports = putAllNotifications;
