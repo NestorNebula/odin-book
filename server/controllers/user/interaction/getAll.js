f5bad2d19fd8b5db581fbd2a31b3f05d24637c71
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 16:38:23 +0100

    feat: add getAllInteractions controller

--- server/controllers/user/interaction/getAll.js
+++ server/controllers/user/interaction/getAll.js
@@ -1,3 +1,24 @@
-const getAllInteractions = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const getAllInteractions = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(new Sperror('Forbidden', "Can't access this data.", 403));
+  }
+  if (req.query.type !== 'LIKE' && req.query.type !== 'BOOKMARK') {
+    return next(
+      new Sperror(
+        'Invalid query argument("type")',
+        'Type must be "LIKE" or "BOOKMARK"',
+        400
+      )
+    );
+  }
+  const interactions = await prisma.findInteractions(
+    req.user.id,
+    req.query.type
+  );
+  res.json({ interactions });
+};
 
 module.exports = getAllInteractions;
