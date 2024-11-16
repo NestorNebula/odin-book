d4a2771ac58718b9fb533fb66774078ed7cd2395
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 16:10:38 +0100

    feat: add reposts in getAllFollowingPosts

--- server/controllers/user/following/post/getAll.js
+++ server/controllers/user/following/post/getAll.js
@@ -6,7 +6,8 @@ const getAllFollowingPosts = async (req, res, next) => {
     return next(new Sperror('Forbidden', "You can't access this data.", 403));
   }
   const posts = await prisma.findUserFollowingPosts(req.user.id);
-  res.json({ posts });
+  const reposts = await prisma.findUserFollowingReposts(req.user.id);
+  res.json({ posts, reposts });
 };
 
 module.exports = getAllFollowingPosts;
