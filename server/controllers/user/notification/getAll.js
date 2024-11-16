fe9523ea8d38ee4a1f564bea0847a87f94e1f870
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 10:04:31 +0100

    feat: add getAllNotifications controller

--- server/controllers/user/notification/getAll.js
+++ server/controllers/user/notification/getAll.js
@@ -1,3 +1,12 @@
-const getAllNotifications = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const getAllNotifications = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(new Sperror('Forbidden', "You can't access this data.", 403));
+  }
+  const notifications = await prisma.findNotifications(req.user.id);
+  res.json({ notifications });
+};
 
 module.exports = getAllNotifications;
