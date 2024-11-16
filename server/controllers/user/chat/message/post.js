5b7e7380c74648832257847625ada9b325b7c79d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 12:26:06 +0100

    feat: add postMessage controller

--- server/controllers/user/chat/message/post.js
+++ server/controllers/user/chat/message/post.js
@@ -1,3 +1,43 @@
-const postMessage = () => {};
+const { validateMessage } = require('../../../../helpers/validation');
+const { validationResult } = require('express-validator');
+const prisma = require('../../../../models/queries');
+const Sperror = require('sperror');
+
+const postMessage = [
+  validateMessage,
+  async (req, res, next) => {
+    if (req.user.id !== +req.params.userId) {
+      return next(
+        new Sperror(
+          'Forbidden',
+          "You can't create a message for another user.",
+          403
+        )
+      );
+    }
+    const userChats = await prisma.findChats(req.user.id);
+    if (!userChats.some((chat) => chat.id === +req.params.chatId)) {
+      return next(
+        new Sperror('Bad Request', "Can't send a message in this chat.", 400)
+      );
+    }
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    const message = await prisma.createMessage(
+      req.user.id,
+      +req.params.chatId,
+      req.body.content.length ? req.body.content : null,
+      req.body.file.length ? req.body.file : null
+    );
+    if (!message) {
+      return next(
+        new Sperror('Server Error', 'Error when creating message.', 500)
+      );
+    }
+    res.json({ message });
+  },
+];
 
 module.exports = postMessage;
