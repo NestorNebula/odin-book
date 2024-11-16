1aaba3544dfbe32697515148e8c65ff70c8e77f6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 10:19:07 +0100

    feat: add tests for putAllNotifications

---
+++ server/controllers/user/notification/putAll.test.js
@@ -0,0 +1,16 @@
+const { request, app, data } = require('../../../tests/setup');
+const router = require('../../../routes/user');
+
+describe('putAllNotifications', () => {
+  it('returns updated notifications', async () => {
+    request(app)
+      .put(`/${data.users[0].id}/notifications`)
+      .then((res) => {
+        expect(res.notifications.every((n) => !!n.seen)).toBeTruthy();
+      });
+  });
+
+  it("returns 403 when trying to update other user's notifications", (done) => {
+    request(app).put(`/${data.users[1].id}/notifications`).expect(403, done);
+  });
+});
