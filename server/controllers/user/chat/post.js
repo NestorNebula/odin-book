a87ea67a46bac341fd81b2d7620f7e1377f17d30
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 11:15:04 +0100

    feat: add postChat controller

--- server/controllers/user/chat/post.js
+++ server/controllers/user/chat/post.js
@@ -1,3 +1,25 @@
-const postChat = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const postChat = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(
+      new Sperror('Forbidden', "Can't create chat for another user.", 403)
+    );
+  }
+  const userToAdd = +req.body.userId
+    ? await prisma.findUserById(+req.body.userId)
+    : null;
+  if (!userToAdd) {
+    return next(
+      new Sperror('Bad Request', "The requested user doesn't exist", 400)
+    );
+  }
+  const chat = await prisma.createChat(req.user.id, userToAdd.id);
+  if (!chat) {
+    return next(new Sperror('Server Error', "Couldn't create chat", 500));
+  }
+  res.json({ chat });
+};
 
 module.exports = postChat;
