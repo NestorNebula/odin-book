53b5b5dbea2e5738cfc79a7913c9809657e5722b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 10:54:34 +0100

    feat: add getAllChats controller

--- server/controllers/user/chat/getAll.js
+++ server/controllers/user/chat/getAll.js
@@ -1,3 +1,12 @@
-const getAllChats = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const getAllChats = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(new Sperror('Forbidden', "You can't access this data.", 403));
+  }
+  const chats = await prisma.findChats(req.user.id);
+  res.json({ chats });
+};
 
 module.exports = getAllChats;
