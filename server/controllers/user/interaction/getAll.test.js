8e4684abc17a481ed15fc3a8fcf891dcee9e0a5f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-16 16:28:51 +0100

    test: add test for getAll interactions

---
+++ server/controllers/user/interaction/getAll.test.js
@@ -0,0 +1,39 @@
+const { request, app, data } = require('../../../tests/setup');
+const router = require('../../../routes/user');
+
+beforeAll(() => {
+  app.use('/', router);
+});
+
+describe('getAllInteractions', () => {
+  it("returns all user's likes", async () => {
+    const user = data.users[0];
+    return request(app)
+      .get(`/${user.id}/interactions`)
+      .query({ type: 'LIKE' })
+      .then((res) => {
+        expect(res.interactions.length).toBe(
+          user.interactions.filter((i) => i.type === 'LIKE').length
+        );
+      });
+  });
+
+  it("returns all user's bookmarks", async () => {
+    const user = data.users[0];
+    return request(app)
+      .get(`/${user.id}/interactions`)
+      .query({ type: 'BOOKMARK' })
+      .then((res) => {
+        expect(res.interactions.length).toBe(
+          user.interactions.filter((i) => i.type === 'BOOKMARK').length
+        );
+      });
+  });
+
+  it("returns 403 when trying to access to other user's interactions", (done) => {
+    request(app)
+      .get(`/${data.users[1].id}/interactions`)
+      .query({ type: 'LIKE' })
+      .expect(403, done);
+  });
+});
