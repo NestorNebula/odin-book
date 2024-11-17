392ef44e96ce68cef44e2a0d8f1ec39ce9529d90
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-17 17:18:53 +0100

    feat: add getPost controller

--- server/controllers/post/get.js
+++ server/controllers/post/get.js
@@ -1,3 +1,24 @@
-const getPost = () => {};
+const prisma = require('../../models/queries');
+const Sperror = require('sperror');
+
+const getPost = async (req, res, next) => {
+  const post = {
+    main: await prisma.findPost(+req.params.postId),
+    next: null,
+  };
+  if (!post.main) {
+    return next(new Sperror('Not Found', "Couldn't found the post.", 404));
+  }
+  let actualPost = post;
+  while (actualPost.main.commentedPostId) {
+    const nextPost = {
+      main: await prisma.findPost(actualPost.main.commentedPostId),
+      next: null,
+    };
+    actualPost.next = nextPost;
+    actualPost = nextPost;
+  }
+  res.json({ post });
+};
 
 module.exports = getPost;
