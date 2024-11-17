d759be3843ba6bb8501eba44f2b6f8f289c9c111
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-17 10:33:09 +0100

    feat: add getAllPosts controller

--- server/controllers/user/post/getAll.js
+++ server/controllers/user/post/getAll.js
@@ -1,3 +1,17 @@
-const getAllPosts = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const getAllPosts = async (req, res, next) => {
+  const user = await prisma.findUserById(+req.params.userId);
+  if (!user) {
+    return next(new Sperror('Not Found', "User doesn't exist", 404));
+  }
+  const posts = await prisma.findUserPosts(user.id);
+  const reposts = await prisma.findUserReposts(user.id);
+  if (!posts || !reposts) {
+    return next(new Sperror('Server Error', 'Error when fetching posts.', 500));
+  }
+  res.json({ posts, reposts });
+};
 
 module.exports = getAllPosts;
