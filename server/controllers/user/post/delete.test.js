82d1bfe61e6e8ff7502b766c42a944a83ecabd94
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-17 14:50:59 +0100

    test: use post id instead of post object

--- server/controllers/user/post/delete.test.js
+++ server/controllers/user/post/delete.test.js
@@ -7,7 +7,7 @@ describe('deletePost', () => {
   it('returns deleted post', async () => {
     const user = data.users[0];
     return request(app)
-      .delete(`/${user.id}/posts/${user.posts[0]}`)
+      .delete(`/${user.id}/posts/${user.posts[0].id}`)
       .then((res) => {
         expect(res.body.post.content).toBe(user.posts[0].content);
       });
@@ -15,7 +15,7 @@ describe('deletePost', () => {
 
   it("returns 403 when trying to delete someone else's post", (done) => {
     request(app)
-      .delete(`/${data.users[1].id}/posts/${data.users[1].posts[0]}`)
+      .delete(`/${data.users[1].id}/posts/${data.users[1].posts[0].id}`)
       .expect(403, done);
   });
 });
