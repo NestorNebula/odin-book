6e78b63070ddb2b98edffb42664941d83a7bcc78
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-17 15:09:15 +0100

    feat: add deletePost controller

--- server/controllers/user/post/delete.js
+++ server/controllers/user/post/delete.js
@@ -1,3 +1,28 @@
-const deletePost = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+const {
+  deletePostCommentsRecursively,
+} = require('../../../helpers/post-recursion');
+
+const deletePost = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(new Sperror('Forbidden', "You can't delete this data.", 403));
+  }
+  if (!req.params.postId || isNaN(+req.params.postId)) {
+    return next(new Sperror('Bad Request', 'No post to delete.', 400));
+  }
+  const post = await prisma.findPost(+req.params.postId);
+  if (!post || post.userId !== req.user.id) {
+    return next(
+      new Sperror(
+        'Bad Request',
+        "The post doesn't exist/can't be deleted.",
+        400
+      )
+    );
+  }
+  const deletedPost = await deletePostCommentsRecursively(post);
+  res.json({ post: deletedPost });
+};
 
 module.exports = deletePost;
