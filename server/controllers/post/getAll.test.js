1edf07a17c956304185061a660c3aaad9d21707d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-17 15:41:43 +0100

    test: add tests for getAllPosts

---
+++ server/controllers/post/getAll.test.js
@@ -0,0 +1,40 @@
+const { request, app, data } = require('../../tests/setup');
+const router = require('../../routes/post');
+
+beforeAll(() => app.use('/', router));
+
+describe('getAllPosts', () => {
+  it('returns all posts', async () => {
+    return request(app)
+      .get('/')
+      .then((res) => {
+        expect(res.posts.length).toBe(data.posts.length);
+      });
+  });
+
+  it("doesn't return more posts than limit query", async () => {
+    return request(app)
+      .get('/')
+      .query({ limit: 2 })
+      .then((res) => {
+        expect(res.body.posts.length).toBeLessThanOrEqual(2);
+      });
+  });
+
+  it('returns posts matching the search query', async () => {
+    const search = data.posts[0].content.split(' ')[0];
+    const regExp = new RegExp(search, 'i');
+    return request(app)
+      .get('/')
+      .query({ search })
+      .then((res) => {
+        expect(res.posts.length).toBe(
+          data.posts.filter(
+            (p) =>
+              regExp.test(p.content) ||
+              regExp.test(data.users.find((u) => u.id === p.userId).username)
+          ).length
+        );
+      });
+  });
+});
