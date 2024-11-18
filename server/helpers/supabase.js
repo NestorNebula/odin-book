87405ab6a9202248bbb32a44aaea9e027eb50654
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-18 10:16:35 +0100

    feat: add uploadFile function

---
+++ server/helpers/supabase.js
@@ -0,0 +1,33 @@
+import { createClient } from '@supabase/supabase-js';
+require('dotenv').config();
+
+const supabase = createClient(
+  process.env.SUPABASE_PROJECT_URL,
+  process.env.SUPABASE_API_KEY
+);
+
+const uploadFile = async (file, path) => {
+  const { data, error } = await supabase.storage
+    .from('images')
+    .upload(path, file);
+  if (error || !data || !data.path || !data.fullPath) {
+    return {
+      link: null,
+      error: true,
+    };
+  } else {
+    const { data } = supabase.storage.from('images').getPublicUrl(path);
+    if (!data || !data.publicUrl) {
+      return {
+        link: null,
+        error: true,
+      };
+    }
+    return {
+      link: data.publicUrl,
+      error: false,
+    };
+  }
+};
+
+module.exports = { uploadFile };
