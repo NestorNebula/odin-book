d6c97a0f0f4ad77279d4928a97ee4874073b4189
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-18 11:48:21 +0100

    feat: add postImage controller

---
+++ server/controllers/user/image/post.js
@@ -0,0 +1,41 @@
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+const { uploadFile } = require('../../../helpers/supabase.js');
+const crypto = require('node:crypto');
+
+const postImage = async (req, res, next) => {
+  if (req.user.id !== +req.params.userId) {
+    return next(
+      new Sperror('Forbidden', "You can't upload a file for someone else.", 403)
+    );
+  }
+  const user = await prisma.findUserById(+req.params.userId);
+  if (!user) {
+    return next(new Sperror('Bad Request', "The user doesn't exist.", 400));
+  }
+  if (!req.body.file || !req.body.fileName) {
+    return next(
+      new Sperror('Bad Request', 'Please provide a file and a file name.', 400)
+    );
+  }
+  const types = ['avatars', 'backgrounds', 'photos'];
+  if (!types.includes(req.body.type)) {
+    return next(
+      new Sperror('Bad Request', 'Please provide a correct file type.', 400)
+    );
+  }
+  const { link, error } = await uploadFile(
+    req.body.file,
+    `${req.body.type}/${req.body.fileName}-${user.username}-${crypto
+      .randomBytes(10)
+      .toString('hex')}`
+  );
+  if (error) {
+    return next(
+      new Sperror('Server Error', 'Error when uploading image.', 500)
+    );
+  }
+  res.json({ url: link });
+};
+
+module.exports = postImage;
