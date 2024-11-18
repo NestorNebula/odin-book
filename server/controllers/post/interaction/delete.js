ddc5e21699f3cfabc501c5a87088264b36764289
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-18 08:20:11 +0100

    feat: add deleteInteraction controller

--- server/controllers/post/interaction/delete.js
+++ server/controllers/post/interaction/delete.js
@@ -1,3 +1,35 @@
-const deleteInteraction = () => {};
+const prisma = require('../../../models/queries');
+const Sperror = require('sperror');
+
+const deleteInteraction = async (req, res, next) => {
+  const interactionsType = ['LIKE', 'REPOST', 'BOOKMARK'];
+  if (!interactionsType.includes(req.body.type)) {
+    return next(
+      new Sperror(
+        'Bad Request',
+        'Please provide an existing interaction type',
+        400
+      )
+    );
+  }
+  const interactions = await prisma.findInteractions(
+    req.user.id,
+    req.body.type
+  );
+  if (
+    interactions.some(
+      (interaction) => interaction.postId === +req.params.postId
+    )
+  ) {
+    const interaction = await prisma.deleteInteraction(
+      +req.params.postId,
+      req.user.id,
+      req.body.type
+    );
+    return res.json({ interaction });
+  } else {
+    res.json({ interaction: null });
+  }
+};
 
 module.exports = deleteInteraction;
