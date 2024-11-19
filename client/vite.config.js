3c45dab6836430120e270b72ba7b7f6b053cae7a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-19 16:43:24 +0100

    build: update aliases configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -10,14 +11,15 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@actions': './src/actions',
-      '@assets': './src/assets',
-      '@components': './src/components',
-      '@hooks': './src/hooks',
-      '@loaders': './src/loaders',
-      '@pages': './src/pages',
-      '@routes': './src/routes',
-      '@services': './src/services',
+      '@': path.resolve(__dirname, './src'),
+      '@actions': path.resolve(__dirname, './src/actions'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@loaders': path.resolve(__dirname, './src/loaders'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@services': path.resolve(__dirname, './src/services'),
     },
   },
 });
