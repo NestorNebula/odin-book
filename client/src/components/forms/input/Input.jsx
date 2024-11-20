a5416c0b358d5d7a368050e864a3214bd38fa874
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-20 15:33:32 +0100

    feat: add Input component

--- client/src/components/forms/input/Input.jsx
+++ client/src/components/forms/input/Input.jsx
@@ -1,3 +1,44 @@
-function Input() {}
+import PropTypes from 'prop-types';
+import * as S from './Input.styles';
+
+function Input({
+  name,
+  value,
+  updateValue,
+  validation,
+  label = name,
+  type = 'text',
+  optional,
+}) {
+  return (
+    <S.Container>
+      <S.Input
+        id={name}
+        name={name}
+        value={value}
+        onChange={updateValue}
+        type={type}
+        required={!optional}
+      />
+      <S.Label htmlFor={name}>{label}</S.Label>
+      {!!validation.maxLength && (
+        <S.DivMaxLength>
+          {value.length} / {validation.maxLength}
+        </S.DivMaxLength>
+      )}
+      {!validation.isValid && <S.Error>{validation.message}</S.Error>}
+    </S.Container>
+  );
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  updateValue: PropTypes.func.isRequired,
+  validation: PropTypes.object.isRequired,
+  label: PropTypes.string,
+  type: PropTypes.string,
+  optional: PropTypes.bool,
+};
 
 export default Input;
---
+++ client/src/components/forms/input/Input.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div``;
+const Input = styled.input``;
+const Label = styled.label``;
+const DivMaxLength = styled.div``;
+const Error = styled.div``;
+
+export { Container, Input, Label, DivMaxLength, Error };
