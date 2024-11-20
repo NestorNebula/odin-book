c0302d98585d36c8c5a04782c93da37597c7a257
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-20 16:34:25 +0100

    feat: add useInput hook

---
+++ client/src/hooks/useInput.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const useInput = ({ initialValue = '', validate }) => {
+  const [value, setValue] = useState(initialValue);
+  const [validation, setValidation] = useState({ isValid: true });
+
+  const updateValue = (e) => {
+    const { success: preSuccess, value: preUpdatedValue } = validate.before(
+      e.target.value
+    );
+    if (!preSuccess) return;
+    setValue(preUpdatedValue);
+    const { success, message } = validate.after(e.target.value);
+    setValidation(success ? { isValid: true } : { isValid: false, message });
+  };
+
+  return { value, validation, updateValue };
+};
+
+export { useInput };
