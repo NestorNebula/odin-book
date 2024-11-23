34613532553735213fc1f7eb23530df32a6f1870
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-23 11:17:01 +0100

    feat: add SignIn page content

--- client/src/pages/signin/SignIn.jsx
+++ client/src/pages/signin/SignIn.jsx
@@ -1,3 +1,95 @@
-function SignIn() {}
+import { useAuth, useDialog } from '@hooks';
+import { Navigate, useSearchParams } from 'react-router-dom';
+import { Button, Dialog } from '@components/elements';
+import SignUpForm from './signupform/SignUpForm';
+import LoginForm from './loginform/LoginForm';
+import icon from '@assets/icons/icon.svg';
+import * as S from './SignIn.styles';
+import ghIcon from '@assets/icons/github.svg';
+const API_URL = import.meta.env.VITE_API_URL;
+
+function SignIn() {
+  const { done, method, setMethod, errors, methods } = useAuth();
+  const { dialogRef, open, close } = useDialog();
+  const updateMethod = (m, openForm) => {
+    setMethod(m);
+    openForm ? open() : close();
+  };
+
+  const [searchParams] = useSearchParams();
+  const fail = searchParams.get('fail') !== null;
+
+  return (
+    <S.SignIn>
+      {!!done && <Navigate to="/" />}
+      <Dialog.Main dialogRef={dialogRef}>
+        {open ? (
+          method === 'signup' ? (
+            <>
+              <Dialog.Header>
+                <Dialog.CloseButton close={close} />
+                <div>
+                  <img src={icon} alt="" />
+                  <div>Create your account</div>
+                  <div></div>
+                </div>
+              </Dialog.Header>
+              <SignUpForm onSubmit={methods.signUp} />
+            </>
+          ) : method === 'login' ? (
+            <>
+              <Dialog.Header>
+                <Dialog.CloseButton close={close} />
+                <div>
+                  <img src={icon} alt="" />
+                  <div>Log In to Odin-Book</div>
+                  <div></div>
+                </div>
+              </Dialog.Header>
+              <LoginForm onSubmit={methods.logIn} />
+            </>
+          ) : null
+        ) : null}
+      </Dialog.Main>
+      <S.Icon src={icon} alt="Odin-Book" />
+      <S.Section>
+        <S.SectionHeader>
+          <div>Happening now</div>
+          <div>Join today.</div>
+        </S.SectionHeader>
+        <S.Methods>
+          <div>
+            <Button onClick={close}>
+              <a href={`${API_URL}/auth/signin/github`}>
+                <img src={ghIcon} alt="" />
+                <div>Sign In with GitHub</div>
+              </a>
+            </Button>
+          </div>
+          <div>or</div>
+          <div>
+            <Button onClick={() => updateMethod('signup', true)}>
+              Create account
+            </Button>
+            <Button onClick={() => updateMethod('login', true)}>Log In</Button>
+            <Button onClick={() => updateMethod('guest', false)}>
+              Sign In as Guest
+            </Button>
+          </div>
+        </S.Methods>
+      </S.Section>
+      {(!!errors.length || fail) && (
+        <S.Errors>
+          <>
+            {errors.map((e) => (
+              <div key={e.msg}>{e.msg}</div>
+            ))}
+            {fail && <div>Error during Github signin.</div>}
+          </>
+        </S.Errors>
+      )}
+    </S.SignIn>
+  );
+}
 
 export default SignIn;
---
+++ client/src/pages/signin/SignIn.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const SignIn = styled.main``;
+const Icon = styled.img``;
+const Section = styled.section``;
+const SectionHeader = styled.header``;
+const Methods = styled.div``;
+const Errors = styled.div``;
+
+export { SignIn, Icon, Section, SectionHeader, Methods, Errors };
