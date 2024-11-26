ee2adc4865f1ae75cec38967bb14e47b806ede4d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-26 16:48:12 +0100

    feat: simplify dashboardLoader
    The Dashboard component will handle fetching the user data
    Letting the loader do it would have lead to a lack of flexibility for
    updating user's informations during app use

--- client/src/loaders/dashboardLoader.js
+++ client/src/loaders/dashboardLoader.js
@@ -1,21 +1,9 @@
 import { redirect } from 'react-router-dom';
-import { fetchAPI } from '@services';
-import { defaultContext } from '@context';
 
 const dashboardLoader = async () => {
   const userId = localStorage.getItem('id');
   if (!userId) return redirect('/signin');
-  if (userId === '0') return { user: defaultContext };
-  const { result, error } = await fetchAPI({
-    method: 'get',
-    path: `users/${userId}`,
-  });
-  if (error) {
-    localStorage.removeItem('id');
-    return redirect('/signin');
-  }
-
-  return { user: result.user };
+  return { userId };
 };
 
 export default dashboardLoader;
