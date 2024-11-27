10b411ed6c2357e3eb3c69fd3c471c26e0ba4ca3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-27 18:13:28 +0100

    feat: update defaultContext

--- client/src/context/defaultContext.js
+++ client/src/context/defaultContext.js
@@ -1,21 +1,23 @@
 const defaultContext = {
-  id: 0,
-  username: 'guest',
-  email: null,
-  loginMethod: 'GUEST',
-  profile: {
-    userId: 0,
-    displayName: 'Guest',
-    avatar: null,
-    background: null,
-    bio: null,
-    location: null,
-    website: null,
-    creationDate: new Date(Date.now()),
+  user: {
+    id: 0,
+    username: 'guest',
+    email: null,
+    loginMethod: 'GUEST',
+    profile: {
+      userId: 0,
+      displayName: 'Guest',
+      avatar: null,
+      background: null,
+      bio: null,
+      location: null,
+      website: null,
+      creationDate: new Date(Date.now()),
+    },
+    following: [],
+    followers: [],
+    notifications: [],
   },
-  following: [],
-  followers: [],
-  notifications: [],
 };
 
 export default defaultContext;
