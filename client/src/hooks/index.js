524064abbb9fce9a81415606d73fce5d504bfa5b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-27 17:19:31 +0100

    feat: add useUserData hook

--- client/src/hooks/index.js
+++ client/src/hooks/index.js
@@ -1,5 +1,6 @@
 import useAuth from './useAuth';
 import useDialog from './useDialog';
 import useInput from './useInput';
+import useUserData from './useUserData';
 
-export { useAuth, useDialog, useInput };
+export { useAuth, useDialog, useInput, useUserData };
---
+++ client/src/hooks/useUserData.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { defaultContext } from '@context';
+import { fetchAPI } from '@services';
+
+const useUserData = ({ userId }) => {
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [update, setUpdate] = useState(false);
+  const updateUserData = () => {
+    setUpdate(!update);
+  };
+
+  useEffect(() => {
+    if (userId === 0) {
+      setUserData(defaultContext.user);
+      setError(false);
+      setLoading(false);
+    } else {
+      fetchAPI({ method: 'get', path: `users/${userId}` })
+        .then((fetch) => {
+          if (fetch.error) {
+            throw new Error(fetch.result.msg);
+          } else {
+            setUserData(fetch.result.user);
+            setError(false);
+          }
+        })
+        .catch((err) => {
+          setError(err);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [userId, update]);
+
+  return { userData, error, loading, updateUserData };
+};
+
+export default useUserData;
