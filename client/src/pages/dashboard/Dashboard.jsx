3d023b9403a3b525ec5e2829f8371f774f1b01a3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-27 18:16:41 +0100

    feat: add basis structure to Dashboard

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,3 +1,25 @@
-function Dashboard() {}
+import { Outlet, useLoaderData } from 'react-router-dom';
+import { Context } from '@context';
+import { useDialog, useUserData } from '@hooks';
+import { Dialog } from '@components/elements';
+import Navbar from './navbar/Navbar';
+import * as S from './Dashboard.styles';
+
+function Dashboard() {
+  const { userId } = useLoaderData();
+  const { userData, error, loading, updateUserData } = useUserData({ userId });
+
+  const { dialogRef, open, close } = useDialog();
+
+  return (
+    <Context.Provider value={{ user: userData }}>
+      <S.Dashboard>
+        <Dialog.Main dialogRef={dialogRef} close={close}></Dialog.Main>
+        <Navbar updateUser={updateUserData} openNewPost={open} />
+        <Outlet />
+      </S.Dashboard>
+    </Context.Provider>
+  );
+}
 
 export default Dashboard;
---
+++ client/src/pages/dashboard/Dashboard.styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Dashboard = styled.main``;
+
+export { Dashboard };
