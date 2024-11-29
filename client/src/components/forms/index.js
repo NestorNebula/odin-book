677368083619f5ba470ee72e7cc77dd11ce18ccd
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-29 11:43:33 +0100

    feat: add PostForm component

--- client/src/components/forms/index.js
+++ client/src/components/forms/index.js
@@ -1,5 +1,6 @@
 import Input from './input/Input';
 import Button from './button/Button';
 import AuthenticationForm from './authenticationform/AuthenticationForm';
+import PostForm from './postform/PostForm';
 
-export { Input, Button, AuthenticationForm };
+export { Input, Button, AuthenticationForm, PostForm };
