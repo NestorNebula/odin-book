888a0b753c3c99a7a9e0698a3faf0d8c4c4cd9db
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-29 11:03:25 +0100

    test: fix useContext mock to be used at right time

--- client/src/pages/dashboard/navbar/Navbar.test.jsx
+++ client/src/pages/dashboard/navbar/Navbar.test.jsx
@@ -1,15 +1,18 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
 import Navbar from './Navbar';
 import { testsData } from '@services';
 import { useContext } from 'react';
-
-const updateUser = vi.fn();
 const openNewPost = vi.fn();
 
 beforeEach(() => {
-  render(<Navbar updateUser={updateUser} openNewPost={openNewPost} />);
+  render(
+    <MemoryRouter>
+      <Navbar openNewPost={openNewPost} />
+    </MemoryRouter>
+  );
 });
 
 const mockUser = testsData.fullUser({});
@@ -23,18 +26,19 @@ vi.mock('react', async () => {
   };
 });
 
+const numNotifications = 3;
+
 describe('Navbar', () => {
   it('renders correctly', () => {
     expect(screen.queryByText(`@${mockUser.username}`)).not.toBeNull();
-  });
-
-  it('renders number of not seen notifications', () => {
-    const numNotifications = 3;
     const mockUserWithNotifications = testsData.fullUser({ numNotifications });
     useContext.mockImplementationOnce(() => {
       return { user: mockUserWithNotifications };
     });
-    expect(screen.queryByText(numNotifications)).not.toBeNull();
+  });
+
+  it('renders number of not seen notifications', () => {
+    expect(screen.getByText(numNotifications)).not.toBeNull();
   });
 
   it('renders post button and use onNewPostClick on click', async () => {
