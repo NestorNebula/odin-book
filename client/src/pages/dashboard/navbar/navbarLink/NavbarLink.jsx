d276742beec09ba62202941b3e4b869bb095501f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-29 07:53:54 +0100

    feat: add NavbarLink component

--- client/src/pages/dashboard/navbar/navbarLink/NavbarLink.jsx
+++ client/src/pages/dashboard/navbar/navbarLink/NavbarLink.jsx
@@ -1,3 +1,24 @@
-function NavbarItem() {}
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-export default NavbarItem;
+const StyledNavbarLinkItem = styled.li``;
+
+function NavbarLink({ link, iconSrc, title }) {
+  return (
+    <StyledNavbarLinkItem>
+      <Link to={`/${link}`} aria-label={`Go to ${title} page.`}>
+        <img src={iconSrc} alt="" />
+        <div>{title}</div>
+      </Link>
+    </StyledNavbarLinkItem>
+  );
+}
+
+NavbarLink.propTypes = {
+  link: PropTypes.string.isRequired,
+  iconSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default NavbarLink;
