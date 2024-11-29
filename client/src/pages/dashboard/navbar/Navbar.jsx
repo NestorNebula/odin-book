dc263d17ea68239edfffc4d8f84758b5ba33eab4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-29 11:12:32 +0100

    feat: add content to Navbar component

--- client/src/pages/dashboard/navbar/Navbar.jsx
+++ client/src/pages/dashboard/navbar/Navbar.jsx
@@ -1,3 +1,49 @@
-function Navbar() {}
+import { useContext } from 'react';
+import { Context } from '@context';
+import { Button } from '@components/elements';
+import NavbarLink from './navbarLink/NavbarLink';
+import NavbarProfile from './navbarProfile/NavbarProfile';
+import PropTypes from 'prop-types';
+import * as icons from '@assets/icons';
+import * as S from './Navbar.styles';
+
+function Navbar({ openNewPost }) {
+  const { user } = useContext(Context);
+
+  return (
+    <S.Navbar>
+      <ul>
+        <S.LogoItem>
+          <img src={icons.icon} alt="Odin-Book" />
+        </S.LogoItem>
+        <NavbarLink link="home" iconSrc={icons.home} />
+        <NavbarLink link="explore" iconSrc={icons.explore} />
+        <NavbarLink
+          link="notifications"
+          iconSrc={icons.notification}
+          notificationsNum={user.notifications.reduce(
+            (num, notification) =>
+              notification.seen ? num : num === null ? 1 : ++num,
+            null
+          )}
+        />
+        <NavbarLink link="messages" iconSrc={icons.message} />
+        <NavbarLink link="bookmarks" iconSrc={icons.bookmark} />
+        <NavbarLink
+          link={user.loginMethod !== 'GUEST' ? `${user.id}` : 'guest'}
+          iconSrc={icons.profile}
+          title="profile"
+        />
+        <NavbarLink link="settings" iconSrc={icons.settings} />
+        <Button onClick={openNewPost}>Post</Button>
+        <NavbarProfile username={user.username} profile={user.profile} />
+      </ul>
+    </S.Navbar>
+  );
+}
+
+Navbar.propTypes = {
+  openNewPost: PropTypes.func.isRequired,
+};
 
 export default Navbar;
---
+++ client/src/pages/dashboard/navbar/Navbar.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const Navbar = styled.nav``;
+const LogoItem = styled.li``;
+
+export { Navbar, LogoItem };
