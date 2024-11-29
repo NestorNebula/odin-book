7b5a4f8f8af6f76ba0a4370cb294cf2d6c2b3841
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-29 09:04:19 +0100

    feat: add index file to export assets

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,25 @@
+import avatar from './avatar.png';
+import bookmark from './bookmark.svg';
+import close from './close.svg';
+import explore from './explore.svg';
+import github from './github.svg';
+import home from './home.svg';
+import icon from './icon.svg';
+import message from './message.svg';
+import notification from './notification.svg';
+import profile from './profile.svg';
+import settings from './settings.svg';
+
+export {
+  avatar,
+  bookmark,
+  close,
+  explore,
+  github,
+  home,
+  icon,
+  message,
+  notification,
+  profile,
+  settings,
+};
