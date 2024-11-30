57dc1e74c7a99a13315272815976e3a47851db52
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-30 09:11:45 +0100

    feat: add tests for PostForm

---
+++ client/src/components/forms/postform/PostForm.test.jsx
@@ -0,0 +1,66 @@
+import { describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import { PostForm } from '..';
+import { testsData } from '@services';
+import { faker } from '@faker-js/faker';
+
+const mockUser = testsData.fullUser({});
+const mockPost = testsData.post({ details: true });
+const mockSubmit = vi.fn();
+
+describe('PostForm', () => {
+  it('displays user profile picture', () => {
+    render(
+      <MemoryRouter>
+        <PostForm userProfile={mockUser.profile} onSubmit={mockSubmit} />
+      </MemoryRouter>
+    );
+    expect(
+      screen.queryByLabelText(mockUser.profile.displayName)
+    ).not.toBeNull();
+  });
+
+  it('displays normal placeholder for post', () => {
+    render(
+      <MemoryRouter>
+        <PostForm userProfile={mockUser.profile} onSubmit={mockSubmit} />
+      </MemoryRouter>
+    );
+    expect(screen.queryByPlaceholderText(/what is happening/i)).not.toBeNull();
+  });
+
+  it('displays special placeholder for comment', () => {
+    render(
+      <MemoryRouter>
+        <PostForm
+          userProfile={mockUser.profile}
+          onSubmit={mockSubmit}
+          post={mockPost}
+        />
+      </MemoryRouter>
+    );
+    expect(screen.queryByPlaceholderText(/what is happening/i)).toBeNull();
+    expect(screen.queryByPlaceholderText(/post your reply/i)).not.toBeNull();
+  });
+
+  it('calls onSubmit only when content is not empty', async () => {
+    const user = userEvent.setup();
+    const submitBtn = screen.getByRole('button', { name: /post/i });
+    await user.click(submitBtn);
+    expect(mockSubmit).not.toHaveBeenCalled();
+    const content = screen.getByPlaceholderText(/what is happening/i);
+    await user.type(content, faker.lorem.paragraphs(1));
+    await user.click(submitBtn);
+    expect(mockSubmit).toHaveBeenCalled();
+  });
+
+  it('prevents user from typing more than characters limit', async () => {
+    const charactersLimit = 280;
+    const user = userEvent.setup();
+    const content = screen.getByPlaceholderText(/what is happening/i);
+    await user.type(content, faker.lorem.paragraphs(5));
+    expect(content.value.length).toBeLessThanOrEqual(charactersLimit);
+  });
+});
