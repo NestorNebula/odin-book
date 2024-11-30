24281e11ec7d98521fc05e8b6ee8d4ff35565080
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-11-30 11:36:45 +0100

    feat: add disabled prop to form Button

--- client/src/components/forms/button/Button.jsx
+++ client/src/components/forms/button/Button.jsx
@@ -3,13 +3,18 @@ import styled from 'styled-components';
 
 const StyledButton = styled.button``;
 
-function Button({ children, type }) {
-  return <StyledButton type={type}>{children}</StyledButton>;
+function Button({ children, type, disabled }) {
+  return (
+    <StyledButton type={type} disabled={disabled}>
+      {children}
+    </StyledButton>
+  );
 }
 
 Button.propTypes = {
   children: PropTypes.string,
   type: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
 };
 
 export default Button;
