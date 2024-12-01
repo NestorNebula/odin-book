f4afc9ecd93b4427d01b958091acc72155ad19fe
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-01 10:27:45 +0100

    feat: add PostForm in Dashboard's Dialog

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import { Outlet, useLoaderData } from 'react-router-dom';
 import { Context } from '@context';
-import { useDialog, useUserData } from '@hooks';
+import { useDialog, useInformation, useUserData } from '@hooks';
 import { Dialog } from '@components/elements';
+import { PostForm } from '@components/forms';
 import Navbar from './navbar/Navbar';
 import { Error, Loading } from '@components';
+import { fetchAPI } from '@services';
 import * as S from './Dashboard.styles';
 
 function Dashboard() {
@@ -11,6 +13,26 @@ function Dashboard() {
   const { userData, error, loading, updateUserData } = useUserData({ userId });
 
   const { dialogRef, open, close } = useDialog();
+  const { information, updateInformation } = useInformation();
+
+  const submitPost = async ({ content, file }) => {
+    const fetch = await fetchAPI({
+      body: { content, file },
+      method: 'POST',
+      path: `users/${userData.id}/posts`,
+    });
+    if (fetch.error) {
+      updateInformation({
+        error: true,
+        message: fetch.result.error
+          ? fetch.result.error.msg
+          : fetch.result.errors[0].msg,
+      });
+    } else {
+      updateUserData();
+      close();
+    }
+  };
 
   return error ? (
     <Error>Error when loading user data. {error}</Error>
@@ -19,9 +41,17 @@ function Dashboard() {
   ) : (
     <Context.Provider value={{ user: userData }}>
       <S.Dashboard>
-        <Dialog.Main dialogRef={dialogRef} close={close}></Dialog.Main>
+        <Dialog.Main dialogRef={dialogRef} close={close}>
+          <Dialog.Header>
+            <Dialog.CloseButton close={close} />
+          </Dialog.Header>
+          <PostForm onSubmit={submitPost} />
+        </Dialog.Main>
         <Navbar updateUser={updateUserData} openNewPost={open} />
         <Outlet />
+        {!!information.message && (
+          <S.Information>{information.message}</S.Information>
+        )}
       </S.Dashboard>
     </Context.Provider>
   );
--- client/src/pages/dashboard/Dashboard.styles.js
+++ client/src/pages/dashboard/Dashboard.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
 const Dashboard = styled.main``;
+const Information = styled.div``;
 
-export { Dashboard };
+export { Dashboard, Information };
