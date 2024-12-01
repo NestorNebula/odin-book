8f34da560b355b1d9083afe8c95928ef070a5448
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-01 10:47:50 +0100

    feat: add max size limit to app

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
 app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
