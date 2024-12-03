ef2d16ee660093a4db22b753bbaa714ff9736317
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-03 08:14:44 +0100

    feat: add user's mock in test setup

--- client/test-setup.js
+++ client/test-setup.js
@@ -1,6 +1,7 @@
 import { afterEach, beforeAll, expect, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
+import { testsData } from '@services';
 
 expect.extend(matchers);
 
@@ -14,6 +15,17 @@ beforeAll(() => {
   });
   HTMLDialogElement.prototype.close = vi.fn(function () {
     this.open = false;
+  });
+
+  const mockUser = testsData.fullUser();
+  vi.mock('react', async () => {
+    const actual = await vi.importActual('react');
+    return {
+      ...actual,
+      useContext: vi.fn(() => {
+        return { user: mockUser };
+      }),
+    };
   });
 });
 
