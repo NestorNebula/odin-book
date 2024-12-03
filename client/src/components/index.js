81feabe117d3926f9e6a7f16b510b71f659a0e61
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-03 13:47:36 +0100

    fix: add missing export for Post component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Avatar from './avatar/Avatar';
 import Error from './Error';
 import Loading from './Loading';
+import Post from './post/Post';
 
-export { Avatar, Error, Loading };
+export { Avatar, Error, Loading, Post };
