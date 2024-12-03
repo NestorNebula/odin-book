f04f38780480d3ed41e826b3ebcf60d9d46f2b11
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-03 11:57:36 +0100

    feat: add Post component

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,3 +1,92 @@
-function Post() {}
+import { useContext } from 'react';
+import { Context } from '@context';
+import { Avatar } from '@components';
+import { Button } from '@components/elements';
+import { date } from '@services';
+import PropTypes from 'prop-types';
+import {
+  comment,
+  repost,
+  emptyHeart,
+  heart,
+  emptyBookmark,
+  bookmark,
+} from '@assets/icons';
+import * as S from './Post.styles';
+
+function Post({
+  post,
+  details,
+  onReplyClick,
+  onRepostClick,
+  onLikeClick,
+  onBookmarkClick,
+}) {
+  const { user } = useContext(Context);
+  const hasReposted = !!post.interactions.find(
+    (i) => i.type === 'REPOST' && i.userId === user.id
+  );
+  const hasLiked = !!post.interactions.find(
+    (i) => i.type === 'LIKE' && i.userId === user.id
+  );
+  const hasBookmarked = !!post.interactions.find(
+    (i) => i.type === 'BOOKMARK' && i.userId === user.id
+  );
+
+  return (
+    <S.Post>
+      <Avatar profile={post.user.profile} />
+      <S.Header>
+        <div>{post.user.profile.displayName}</div>
+        <div>@{post.user.username}</div>
+        {!details ? <div>{date.getDate(post.creationDate)}</div> : null}
+      </S.Header>
+      <S.Content>
+        {!!post.content && <div>{post.content}</div>}
+        {!!post.file && <img src={post.file} />}
+        {!!details && <div>{date.getFullDate(post.creationDate)}</div>}
+      </S.Content>
+      <S.Buttons>
+        <Button onClick={() => onReplyClick(post.id)}>
+          <img src={comment} alt="reply" />
+          <div>{post.comments.length}</div>
+        </Button>
+        <Button onClick={() => onRepostClick(post.id)}>
+          <img src={repost} alt={hasReposted ? 'undo repost' : 'repost'} />
+          <div>
+            {post.interactions.filter((i) => i.type === 'REPOST').length}
+          </div>
+        </Button>
+        <Button onClick={() => onLikeClick(post.id)}>
+          <img
+            src={hasLiked ? heart : emptyHeart}
+            alt={hasLiked ? 'unlike' : 'like'}
+          />
+          <div>{post.interactions.filter((i) => i.type === 'LIKE').length}</div>
+        </Button>
+        <Button onClick={() => onBookmarkClick(post.id)}>
+          <img
+            src={hasBookmarked ? bookmark : emptyBookmark}
+            alt={hasBookmarked ? 'bookmark' : 'remove from bookmarks'}
+          />
+          {!!details && (
+            <div>
+              {post.interactions.filter((i) => i.type === 'BOOKMARK').length}
+            </div>
+          )}
+        </Button>
+      </S.Buttons>
+    </S.Post>
+  );
+}
+
+Post.propTypes = {
+  post: PropTypes.object.isRequired,
+  details: PropTypes.bool,
+  onReplyClick: PropTypes.func.isRequired,
+  onRepostClick: PropTypes.func.isRequired,
+  onLikeClick: PropTypes.func.isRequired,
+  onBookmarkClick: PropTypes.func.isRequired,
+};
 
 export default Post;
---
+++ client/src/components/post/Post.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Post = styled.div``;
+const Header = styled.header``;
+const Content = styled.div``;
+const Buttons = styled.div``;
+
+export { Post, Header, Content, Buttons };
