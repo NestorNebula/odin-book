031759218c23806f72dda073d9acd46513e75859
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-03 11:27:37 +0100

    feat: add post's interactions icons

--- client/src/assets/icons/index.js
+++ client/src/assets/icons/index.js
@@ -1,27 +1,37 @@
 import avatar from './avatar.png';
 import bookmark from './bookmark.svg';
 import close from './close.svg';
+import comment from './comment.svg';
+import emptyBookmark from './emptybookmark.svg';
+import emptyHeart from './emptyheart.svg';
 import explore from './explore.svg';
 import github from './github.svg';
+import heart from './heart.svg';
 import home from './home.svg';
 import icon from './icon.svg';
 import image from './image.svg';
 import message from './message.svg';
 import notification from './notification.svg';
 import profile from './profile.svg';
+import repost from './repost.svg';
 import settings from './settings.svg';
 
 export {
   avatar,
   bookmark,
   close,
+  comment,
+  emptyBookmark,
+  emptyHeart,
   explore,
   github,
+  heart,
   home,
   icon,
   image,
   message,
   notification,
   profile,
+  repost,
   settings,
 };
