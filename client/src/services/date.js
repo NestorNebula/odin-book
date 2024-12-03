6f999156c1c63215cc2e5755791e041769ed4e37
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-03 10:58:16 +0100

    feat: add date module

---
+++ client/src/services/date.js
@@ -0,0 +1,27 @@
+const {
+  differenceInCalendarYears,
+  differenceInHours,
+  format,
+  formatDistanceToNowStrict,
+} = require('date-fns');
+
+const getDate = (date) => {
+  const now = new Date(Date.now());
+  if (differenceInHours(now, date) >= 24) {
+    if (differenceInCalendarYears(now, date)) {
+      return format(date, 'MMM d, yyyy');
+    } else {
+      return format(date, 'MMM d');
+    }
+  } else {
+    const distance = formatDistanceToNowStrict(date).split(' ');
+    distance[1] = distance[1][0];
+    return distance.join();
+  }
+};
+
+const getFullDate = (date) => {
+  return format(date, 'p ・ MMM d, yyyy');
+};
+
+export { getDate, getFullDate };
--- client/src/services/index.js
+++ client/src/services/index.js
@@ -2,5 +2,6 @@ import fetchAPI from './fetchAPI';
 import validationChains from './validationChains';
 import testsData from './faker';
 import * as file from './file';
+import * as date from './date';
 
-export { fetchAPI, validationChains, testsData, file };
+export { fetchAPI, validationChains, testsData, file, date };
