ad409e9ec8d695b6ffbd5864c8f7d4f6c898c4c8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-04 10:24:47 +0100

    feat: add content to UserList

--- client/src/components/userlist/UserList.jsx
+++ client/src/components/userlist/UserList.jsx
@@ -1,3 +1,29 @@
-function UserList() {}
+import { Avatar, FollowButton } from '@components';
+import PropTypes from 'prop-types';
+import * as S from './UserList.styles';
+
+function UserList({ users, details }) {
+  return (
+    <S.UserList>
+      <div>Who to Follow</div>
+      <S.Users>
+        {users.map((user) => (
+          <S.User key={user.id}>
+            <Avatar profile={user.profile} />
+            <S.DisplayName>{user.profile.displayName}</S.DisplayName>
+            <S.Username>@{user.username}</S.Username>
+            <FollowButton userId={user.id} />
+            {details && <S.Bio>{user.profile.bio}</S.Bio>}
+          </S.User>
+        ))}
+      </S.Users>
+    </S.UserList>
+  );
+}
+
+UserList.propTypes = {
+  users: PropTypes.array.isRequired,
+  details: PropTypes.bool,
+};
 
 export default UserList;
---
+++ client/src/components/userlist/UserList.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const UserList = styled.div``;
+const Users = styled.div``;
+const User = styled.div``;
+const DisplayName = styled.div``;
+const Username = styled.div``;
+const Bio = styled.div``;
+
+export { UserList, Users, User, DisplayName, Username, Bio };
