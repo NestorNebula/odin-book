bcfe1e1808cb22325487f0cce9bcdbe0837ed6e1
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-04 07:58:11 +0100

    feat: add content to SearchBar

--- client/src/components/searchbar/SearchBar.jsx
+++ client/src/components/searchbar/SearchBar.jsx
@@ -1,3 +1,36 @@
-function SearchBar() {}
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { explore } from '@assets/icons';
+import styled from 'styled-components';
+
+const StyledSearchBar = styled.form``;
+
+function SearchBar({ onSubmit }) {
+  const [value, setValue] = useState('');
+
+  return (
+    <StyledSearchBar
+      onSubmit={async (e) => {
+        e.preventDefault();
+        if (!value) return;
+        await onSubmit({ value });
+      }}
+    >
+      <img src={explore} alt="" />
+      <input
+        type="text"
+        name="searchContent"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Search"
+        aria-label="search"
+      />
+    </StyledSearchBar>
+  );
+}
+
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
