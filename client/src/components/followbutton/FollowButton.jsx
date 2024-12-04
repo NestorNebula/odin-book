97ddd06e89406022dba059fba36f05a0a2f7a722
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-04 09:44:21 +0100

    feat: add FollowButton component

---
+++ client/src/components/followbutton/FollowButton.jsx
@@ -0,0 +1,66 @@
+import { useContext } from 'react';
+import { Context } from '@context';
+import { useState } from 'react';
+import { fetchAPI } from '@services';
+import PropTypes from 'prop-types';
+import * as S from './FollowButton.styles';
+
+function FollowButton({ userId }) {
+  const { user, updateUser } = useContext(Context);
+  const isFollowed = user.following.some((u) => u.id === userId);
+  const [isHovered, setHovered] = useState(false);
+
+  const followUser = async () => {
+    return await fetchAPI({
+      body: { userId },
+      method: 'POST',
+      path: `users/${user.id}/following`,
+    });
+  };
+
+  const unfollowUser = async () => {
+    return await fetchAPI({
+      body: { userId },
+      method: 'DELETE',
+      path: `users/${user.id}/following`,
+    });
+  };
+
+  return (
+    <>
+      {isFollowed ? (
+        isHovered ? (
+          <S.UnfollowButton
+            onClick={async () => {
+              const fetch = await unfollowUser();
+              if (!fetch.error) updateUser();
+            }}
+            onMouseLeave={() => setHovered(false)}
+          >
+            Unfollow
+          </S.UnfollowButton>
+        ) : (
+          <S.FollowingButton onMouseOver={() => setHovered(true)}>
+            Following
+          </S.FollowingButton>
+        )
+      ) : (
+        <S.FollowButton
+          onClick={async () => {
+            const fetch = await followUser();
+            if (!fetch.error) updateUser();
+          }}
+        >
+          Follow
+        </S.FollowButton>
+      )}
+    </>
+  );
+}
+
+FollowButton.propTypes = {
+  userId: PropTypes.number.isRequired,
+  update: PropTypes.func.isRequired,
+};
+
+export default FollowButton;
---
+++ client/src/components/followbutton/FollowButton.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const FollowButton = styled.button``;
+const FollowingButton = styled.button``;
+const UnfollowButton = styled.button``;
+
+export { FollowButton, FollowingButton, UnfollowButton };
