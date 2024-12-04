6f0c670235afe485ac93e6cf88d2b994317b6c5a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-04 10:25:54 +0100

    feat: add UserList to index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,6 @@ import FollowButton from './followbutton/FollowButton';
 import Loading from './Loading';
 import Post from './post/Post';
 import SearchBar from './searchbar/SearchBar';
+import UserList from './userlist/UserList';
 
-export { Avatar, Error, FollowButton, Loading, Post, SearchBar };
+export { Avatar, Error, FollowButton, Loading, Post, SearchBar, UserList };
