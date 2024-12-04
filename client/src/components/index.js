73790bcb65aa3702bcfc20770a39156475a747bb
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-04 09:45:45 +0100

    feat: add FollowButton and SearchBar to index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,8 @@
 import Avatar from './avatar/Avatar';
 import Error from './Error';
+import FollowButton from './followbutton/FollowButton';
 import Loading from './Loading';
 import Post from './post/Post';
+import SearchBar from './searchbar/SearchBar';
 
-export { Avatar, Error, Loading, Post };
+export { Avatar, Error, FollowButton, Loading, Post, SearchBar };
