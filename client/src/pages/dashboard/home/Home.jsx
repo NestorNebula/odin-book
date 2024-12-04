e5af373fd2cea143f9f0875de10f110891596ee7
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-04 11:41:16 +0100

    feat: add content to Home

--- client/src/pages/dashboard/home/Home.jsx
+++ client/src/pages/dashboard/home/Home.jsx
@@ -1,3 +1,52 @@
-function Home() {}
+import { useContext } from 'react';
+import { Context } from '@context';
+import { usePosts } from '@hooks';
+import { Error, Loading } from '@components';
+import HomeContent from './homecontent/HomeContent';
+import HomeSidebar from './homesidebar/HomeSidebar';
+import styled from 'styled-components';
+
+const StyledHome = styled.main``;
+
+function Home() {
+  const { user } = useContext(Context);
+  const {
+    posts,
+    updatePost,
+    error: postsError,
+    loading: postsLoading,
+  } = usePosts({ postsPath: 'posts' });
+  const {
+    posts: followingPosts,
+    reposts,
+    updatePost: updateFollowinPost,
+    updateRepost,
+    error: followingError,
+    loading: followingLoading,
+  } = usePosts({
+    postsPath: `users/${user.id}/following/posts`,
+    fetchReposts: true,
+  });
+
+  return (
+    <StyledHome>
+      {postsError || followingError ? (
+        <Error>Error when loading posts.</Error>
+      ) : postsLoading || followingLoading ? (
+        <Loading data="posts" />
+      ) : (
+        <HomeContent
+          content={{ posts, followingPosts, reposts }}
+          updateContent={{
+            posts: updatePost,
+            followingPosts: updateFollowinPost,
+            reposts: updateRepost,
+          }}
+        />
+      )}
+      <HomeSidebar />
+    </StyledHome>
+  );
+}
 
 export default Home;
