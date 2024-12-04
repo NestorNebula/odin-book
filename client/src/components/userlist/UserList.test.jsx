f2b25726c797afed5400d60ed2a98753296a794a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-04 10:02:42 +0100

    test: remove test for updating data
    The data will be updated by the FollowButton component

--- client/src/components/userlist/UserList.test.jsx
+++ client/src/components/userlist/UserList.test.jsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import UserList from './UserList';
@@ -9,25 +9,16 @@ const users = [];
 for (let i = 0; i < usersNumber; i++) {
   users.push(testsData.fullUser({ userId: i }));
 }
-const mockUpdate = vi.fn();
 
 describe('UserList', () => {
   it('renders every user', () => {
-    render(<UserList users={users} update={mockUpdate} />);
+    render(<UserList users={users} />);
     expect(screen.queryAllByRole('button', { name: /follow/i }).length).toBe(
       users.length
     );
   });
 
   it('displays users bio when details is set to true', () => {
-    render(<UserList users={users} details={true} update={mockUpdate} />);
-  });
-
-  it('calls update after following/unfollowing user', async () => {
-    render(<UserList users={users} update={mockUpdate} />);
-    const user = userEvent.setup();
-    const followBtn = screen.getAllByRole('button', { name: /follow/i })[0];
-    await user.click(followBtn);
-    expect(mockUpdate).toHaveBeenCalled();
+    render(<UserList users={users} details={true} />);
   });
 });
