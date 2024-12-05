a6277ad961e2ffe84f6dda9757a15c0608c9bbed
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-05 08:38:42 +0100

    feat: add content to ExploreResult

--- client/src/pages/dashboard/explore/exploreresult/ExploreResult.jsx
+++ client/src/pages/dashboard/explore/exploreresult/ExploreResult.jsx
@@ -1,3 +1,100 @@
-function ExploreResult() {}
+import { useContext, useState } from 'react';
+import { Context } from '@context';
+import { Link, Navigate } from 'react-router-dom';
+import { Post, UserList } from '@components';
+import { Button } from '@components/elements';
+import { postInteraction } from '@services';
+import PropTypes from 'prop-types';
+import * as S from './ExploreResult.styles';
+
+function ExploreResult({ posts, updatePost, users }) {
+  const { user, updateInformation } = useContext(Context);
+  const sections = ['Top', 'Latest', 'People', 'Media'];
+  const [activeSection, setActiveSection] = useState(0);
+  const recentPosts = posts.toSorted((a, b) => b.creationDate - a.creationDate);
+  console.log(recentPosts);
+  const mediaPosts = posts.filter((p) => !!p.file);
+
+  const [postLink, setPostLink] = useState(null);
+  const structure = postInteraction.createStructure(user.id);
+  const onPostClick = async (interaction, postId) => {
+    if (interaction === 'COMMENT') {
+      return setPostLink(`posts/${postId}`);
+    }
+    const postToUpdate = posts.find((post) => post.id === postId);
+    if (!postToUpdate) return;
+    const result = await postInteraction.interact({
+      structure,
+      interaction,
+      postId,
+      remove: postToUpdate.interactions.some(
+        (i) => i.type === interaction && i.userId === user.id
+      ),
+    });
+    if (!result.success) {
+      updateInformation({ error: true, message: result.msg });
+    } else {
+      updatePost(result.post);
+    }
+  };
+
+  return (
+    <S.ExploreResult>
+      {postLink && <Navigate to={postLink} />}
+      <S.Navbar>
+        <ul>
+          {sections.map((section, index) => (
+            <li key={section}>
+              <Button onClick={() => setActiveSection(index)}>{section}</Button>
+            </li>
+          ))}
+        </ul>
+      </S.Navbar>
+      {sections[activeSection] === 'Top' ? (
+        <S.Posts>
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              post={post}
+              onReplyClick={() => onPostClick('COMMENT', post.id)}
+              onRepostClick={() => onPostClick('REPOST', post.id)}
+              onLikeClick={() => onPostClick('LIKE', post.id)}
+              onBookmarkClick={() => onPostClick('BOOKMARK', post.id)}
+            />
+          ))}
+        </S.Posts>
+      ) : sections[activeSection] === 'Latest' ? (
+        <S.Posts>
+          {recentPosts.map((post) => (
+            <Post
+              key={post.id}
+              post={post}
+              onReplyClick={() => onPostClick('COMMENT', post.id)}
+              onRepostClick={() => onPostClick('REPOST', post.id)}
+              onLikeClick={() => onPostClick('LIKE', post.id)}
+              onBookmarkClick={() => onPostClick('BOOKMARK', post.id)}
+            />
+          ))}
+        </S.Posts>
+      ) : sections[activeSection] === 'People' ? (
+        <UserList users={users} details={true} />
+      ) : sections[activeSection] === 'Media' ? (
+        <S.Media>
+          {mediaPosts.map((post) => (
+            <Link key={post.id} to={`posts/${post.id}`}>
+              <img src={post.file} alt="" />
+            </Link>
+          ))}
+        </S.Media>
+      ) : null}
+    </S.ExploreResult>
+  );
+}
+
+ExploreResult.propTypes = {
+  posts: PropTypes.array.isRequired,
+  updatePost: PropTypes.func.isRequired,
+  users: PropTypes.array.isRequired,
+};
 
 export default ExploreResult;
---
+++ client/src/pages/dashboard/explore/exploreresult/ExploreResult.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const ExploreResult = styled.section``;
+const Navbar = styled.nav``;
+const Posts = styled.div``;
+const Media = styled.div``;
+
+export { ExploreResult, Navbar, Posts, Media };
