3ea645c3e61fc99fc56423d09fe8355aa7e80771
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-05 09:35:25 +0100

    feat: add content to Explore

--- client/src/pages/dashboard/explore/Explore.jsx
+++ client/src/pages/dashboard/explore/Explore.jsx
@@ -1,3 +1,45 @@
-function Explore() {}
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { useData, useSearchData } from '@hooks';
+import { Error, Loading, SearchBar, UserList } from '@components';
+import ExploreResult from './exploreresult/ExploreResult';
+import * as S from './Explore.styles';
+
+function Explore() {
+  const [searchParams] = useSearchParams();
+  const [search, setSearch] = useState(searchParams.get('search') ?? null);
+
+  const { posts, updatePost, users, error, loading } = useSearchData(search);
+
+  const {
+    data,
+    error: sidebarError,
+    loading: sidebarLoading,
+  } = useData({
+    path: `users?omit=friends&limit=10`,
+    dependencies: [],
+  });
+
+  return (
+    <S.Explore>
+      <SearchBar onSubmit={({ value }) => setSearch(value)} />
+      {search &&
+        (error ? (
+          <Error>{error}</Error>
+        ) : loading ? (
+          <Loading data="search results" />
+        ) : (
+          <ExploreResult posts={posts} updatePost={updatePost} users={users} />
+        ))}
+      {sidebarError ? (
+        <Error>{sidebarError}</Error>
+      ) : sidebarLoading ? (
+        <Loading data="users to follow" />
+      ) : (
+        <UserList title="Who to follow" users={data.users} />
+      )}
+    </S.Explore>
+  );
+}
 
 export default Explore;
---
+++ client/src/pages/dashboard/explore/Explore.styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Explore = styled.main``;
+
+export { Explore };
