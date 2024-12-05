b5b119ebf36faf2329e1d1a6d2da3ab05bc9eb70
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-05 15:48:21 +0100

    test: update tests to be able to test the dialog

--- client/src/pages/dashboard/messages/newmessagedialog/NewMessageDialog.test.jsx
+++ client/src/pages/dashboard/messages/newmessagedialog/NewMessageDialog.test.jsx
@@ -5,27 +5,41 @@ import NewMessageDialog from './NewMessageDialog';
 
 const mockSubmit = vi.fn();
 beforeEach(() => {
-  render(<NewMessageDialog onSubmit={mockSubmit} />);
+  render(
+    <NewMessageDialog
+      dialog={{ ref: null, close: () => {} }}
+      onSubmit={mockSubmit}
+    />
+  );
 });
 
 describe('NewMessageDialog', () => {
   it("renders user's following as possibilities", () => {
     expect(
-      screen.queryAllByRole('button', { name: /select/i }).length
+      screen.queryAllByRole('button', { name: /select/i, hidden: true }).length
     ).toBeGreaterThan(0);
   });
 
   it("doesn't call mockSubmit when no user is selected", async () => {
     const user = userEvent.setup();
-    const submitBtn = screen.getByRole('button', { name: /next/i });
+    const submitBtn = screen.getByRole('button', {
+      name: /next/i,
+      hidden: true,
+    });
     await user.click(submitBtn);
     expect(mockSubmit).not.toHaveBeenCalled();
   });
 
   it('calls mock submit when a user is selected', async () => {
     const user = userEvent.setup();
-    const submitBtn = screen.getByRole('button', { name: /next/i });
-    const selectBtn = screen.getAllByRole('button', { name: /select/i })[0];
+    const submitBtn = screen.getByRole('button', {
+      name: /next/i,
+      hidden: true,
+    });
+    const selectBtn = screen.getAllByRole('button', {
+      name: /select/i,
+      hidden: true,
+    })[0];
     await user.click(selectBtn);
     await user.click(submitBtn);
     expect(mockSubmit).toHaveBeenCalled();
