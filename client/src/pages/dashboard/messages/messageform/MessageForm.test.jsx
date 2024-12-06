c33dbfd3b4e0241b3640e13a29ba2afd392f0625
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-06 08:25:32 +0100

    test: add test for MessageForm

---
+++ client/src/pages/dashboard/messages/messageform/MessageForm.test.jsx
@@ -0,0 +1,21 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import MessageForm from './MessageForm';
+import { testsData } from '@services';
+
+const mockSubmit = vi.fn();
+beforeEach(() => render(<MessageForm onSubmit={mockSubmit} />));
+
+describe('MessageForm', () => {
+  it('calls onSubmit only with data', async () => {
+    const user = userEvent.setup();
+    const submitBtn = screen.getByRole('button', { name: /send/i });
+    await user.click(submitBtn);
+    expect(mockSubmit).not.toHaveBeenCalled();
+    const input = screen.getByRole('textbox', { name: /messsage/i });
+    await user.type(input, testsData.message().content);
+    await user.click(submitBtn);
+    expect(mockSubmit).toHaveBeenCalled();
+  });
+});
