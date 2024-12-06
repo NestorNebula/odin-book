2258515c24a0227f77d1e4de91790db3b04aa38b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-06 10:00:17 +0100

    refactor: add FileInput component

--- client/src/components/forms/postform/PostForm.jsx
+++ client/src/components/forms/postform/PostForm.jsx
@@ -4,9 +4,9 @@ import { useInput } from '@hooks';
 import { validationChains } from '@services';
 import { Avatar } from '@components';
 import { Button } from '@components/elements';
-import { Button as SubmitButton, TextArea } from '..';
+import { Button as SubmitButton, FileInput, TextArea } from '..';
 import PropTypes from 'prop-types';
-import { image, close } from '@assets/icons';
+import { close } from '@assets/icons';
 import * as S from './PostForm.styles';
 
 function PostForm({ onSubmit, fileUrl, error, updateFile, removeFile, post }) {
@@ -37,20 +37,9 @@ function PostForm({ onSubmit, fileUrl, error, updateFile, removeFile, post }) {
           </Button>
         </S.FileContainer>
       )}
-      <S.FileInputContainer>
-        <S.FileLabel htmlFor="file">
-          <img src={image} alt="upload image for your post" />
-        </S.FileLabel>
-        <S.FileInput
-          id="file"
-          type="file"
-          name="file"
-          value={''}
-          multiple={false}
-          accept="image/*"
-          onChange={(e) => updateFile(user, e.target.files[0], 'photos')}
-        />
-      </S.FileInputContainer>
+      <FileInput
+        onChange={(e) => updateFile(user, e.target.files[0], 'photos')}
+      />
       <SubmitButton
         type={postIsValid ? 'submit' : 'button'}
         disabled={user.loginMethod === 'GUEST'}
--- client/src/components/forms/postform/PostForm.styles.js
+++ client/src/components/forms/postform/PostForm.styles.js
@@ -3,16 +3,5 @@ import styled from 'styled-components';
 const PostForm = styled.form``;
 const FileContainer = styled.div``;
 const File = styled.img``;
-const FileInputContainer = styled.div``;
-const FileLabel = styled.label``;
-const FileInput = styled.input``;
 const Error = styled.div``;
-export {
-  PostForm,
-  FileContainer,
-  File,
-  FileInputContainer,
-  FileLabel,
-  FileInput,
-  Error,
-};
+export { PostForm, FileContainer, File, Error };
