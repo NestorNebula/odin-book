0c350283df08c8a848d47f7bc656bbfc956837aa
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-06 18:09:45 +0100

    feat: add style file for CommentDialog

---
+++ client/src/components/commentdialog/CommentDialog.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const PostCommented = styled.div``;
+const PostHeader = styled.header``;
+const PostContent = styled.div``;
+
+export { PostCommented, PostHeader, PostContent };
