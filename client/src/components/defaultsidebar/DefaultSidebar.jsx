16e579a1b06ee62331a4fe03bd64227233bbb7f3
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-06 16:14:08 +0100

    feat: add content to DefaultSidebar

--- client/src/components/defaultsidebar/DefaultSidebar.jsx
+++ client/src/components/defaultsidebar/DefaultSidebar.jsx
@@ -1,3 +1,31 @@
-function DefaultSidebar() {}
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useData } from '@hooks';
+import { Error, Loading, SearchBar, UserList } from '@components';
+import * as S from './DefaultSidebar.styles';
+
+function DefaultSidebar() {
+  const [searchValue, setSearchValue] = useState(null);
+
+  const { data, error, loading } = useData({
+    path: 'users?omit=friends&limit=20',
+  });
+
+  return (
+    <S.Sidebar>
+      {searchValue && (
+        <Navigate to={`/explore?search=${encodeURIComponent(searchValue)}`} />
+      )}
+      <SearchBar onSubmit={({ value }) => setSearchValue(value)} />
+      {error ? (
+        <Error>{error}</Error>
+      ) : loading ? (
+        <Loading data="users to follow" />
+      ) : (
+        <UserList title="Who to follow" users={data.users} />
+      )}
+    </S.Sidebar>
+  );
+}
 
 export default DefaultSidebar;
