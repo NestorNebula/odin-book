cc9c4de6152e5cf950f81825ad3fec54a7fc2ad4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-07 11:08:17 +0100

    feat: add Bookmarks page to routes

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -3,6 +3,7 @@ import Home from '@pages/dashboard/home/Home';
 import Explore from '@pages/dashboard/explore/Explore';
 import Notifications from '@pages/dashboard/notifications/Notifications';
 import Messages from '@pages/dashboard/messages/Messages';
+import Bookmarks from '@pages/dashboard/bookmarks/Bookmarks';
 import SignIn from '@pages/signin/SignIn';
 import { dashboardLoader, signInLoader } from '@loaders';
 
@@ -27,6 +28,10 @@ const routes = [
       {
         path: 'messages',
         element: <Messages />,
+      },
+      {
+        path: 'bookmarks',
+        element: <Bookmarks />,
       },
     ],
   },
