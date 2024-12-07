70e2fd6ec0ebba5597e98663737ab925a647be97
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-07 17:33:37 +0100

    feat: add icons for ProfileHeader

--- client/src/assets/icons/index.js
+++ client/src/assets/icons/index.js
@@ -1,5 +1,6 @@
 import avatar from './avatar.png';
 import bookmark from './bookmark.svg';
+import calendar from './calendar.svg';
 import check from './check.svg';
 import close from './close.svg';
 import comment from './comment.svg';
@@ -12,6 +13,8 @@ import heart from './heart.svg';
 import home from './home.svg';
 import icon from './icon.svg';
 import image from './image.svg';
+import link from './link.svg';
+import map from './map.svg';
 import message from './message.svg';
 import newMessage from './new-message.svg';
 import notification from './notification.svg';
@@ -24,6 +27,7 @@ import settings from './settings.svg';
 export {
   avatar,
   bookmark,
+  calendar,
   check,
   close,
   comment,
@@ -36,6 +40,8 @@ export {
   home,
   icon,
   image,
+  link,
+  map,
   message,
   newMessage,
   notification,
