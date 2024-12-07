7dd07c6de9983132255cf3c3d20b6c3cf3ef1c6f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-07 10:50:29 +0100

    feat: add content to BookmarksSidebar

--- client/src/pages/dashboard/bookmarks/bookmarkssidebar/BookmarksSidebar.jsx
+++ client/src/pages/dashboard/bookmarks/bookmarkssidebar/BookmarksSidebar.jsx
@@ -1,3 +1,52 @@
-function BookmarksSidebar() {}
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useData } from '@hooks';
+import { Error, Loading, PostDetails } from '@components';
+import { close, extend } from '@assets/icons';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledSidebar = styled.aside``;
+const StyledHeader = styled.header``;
+const StyledButton = styled.button``;
+
+function BookmarksSidebar({ postId, setPostId }) {
+  const [update, setUpdate] = useState(false);
+  const doUpdate = () => {
+    setUpdate(!update);
+  };
+  const { data, error, loading } = useData({
+    path: `posts/${postId}`,
+    dependencies: [update],
+  });
+  const [extendPost, setExtendPost] = useState(false);
+
+  return (
+    <StyledSidebar>
+      <StyledHeader>
+        {extendPost && <Navigate to={`/posts/${postId}`} />}
+        <StyledButton onClick={() => setPostId(null)} aria-label="close post">
+          <img src={close} alt="" />
+        </StyledButton>
+        <div>Post</div>
+        <StyledButton onClick={() => setExtendPost(true)}>
+          <img src={extend} alt="" />
+        </StyledButton>
+      </StyledHeader>
+      {error ? (
+        <Error>{error}</Error>
+      ) : loading ? (
+        <Loading data="post" />
+      ) : (
+        <PostDetails post={data.post} update={doUpdate} />
+      )}
+    </StyledSidebar>
+  );
+}
+
+BookmarksSidebar.propTypes = {
+  postId: PropTypes.number.isRequired,
+  setPostId: PropTypes.func.isRequired,
+};
 
 export default BookmarksSidebar;
