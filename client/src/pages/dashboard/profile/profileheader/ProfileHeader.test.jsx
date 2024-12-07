651bc200d8f6b5def823d72c1d75bda02add3c91
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-07 11:53:56 +0100

    test: add tests for ProfileHeader

---
+++ client/src/pages/dashboard/profile/profileheader/ProfileHeader.test.jsx
@@ -0,0 +1,40 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import ProfileHeader from './ProfileHeader';
+import { testsData } from '@services';
+
+describe('ProfileHeader', () => {
+  it("renders given user's profile", () => {
+    const user = testsData.user({});
+    render(
+      <MemoryRouter>
+        <ProfileHeader user={user} isUser={false} />
+      </MemoryRouter>
+    );
+    expect(screen.queryByText(user.profile.displayName)).not.toBeNull();
+    expect(screen.queryByText(`@${user.username}`)).not.toBeNull();
+  });
+
+  it('renders profile dialog when given user is user', () => {
+    const user = testsData.user({ userId: 48 });
+    render(
+      <MemoryRouter>
+        <ProfileHeader user={user} isUser={true} />
+      </MemoryRouter>
+    );
+    expect(
+      screen.queryByRole('button', { name: /edit profile/i })
+    ).not.toBeNull();
+  });
+
+  it("renders follow button when given user isn't user", () => {
+    const user = testsData.user({});
+    render(
+      <MemoryRouter>
+        <ProfileHeader user={user} isUser={false} />
+      </MemoryRouter>
+    );
+    expect(screen.queryByRole('button', { name: /follow/i })).not.toBeNull();
+  });
+});
