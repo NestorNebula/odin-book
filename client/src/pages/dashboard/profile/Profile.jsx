a13ab573d11e5881dc4079af70f3e5762572dd3a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-08 17:08:01 +0100

    feat: add content to Profile

--- client/src/pages/dashboard/profile/Profile.jsx
+++ client/src/pages/dashboard/profile/Profile.jsx
@@ -1,3 +1,57 @@
-function Profile() {}
+import { useContext, useState } from 'react';
+import { Context } from '@context';
+import { useParams } from 'react-router-dom';
+import { useData, usePosts } from '@hooks';
+import { Error, Loading } from '@components';
+import ProfileHeader from './profileheader/ProfileHeader';
+import ProfileContent from './profilecontent/ProfileContent';
+import * as S from './Profile.styles';
+
+function Profile() {
+  const { user } = useContext(Context);
+  const { userId } = useParams();
+  const isUser = user.id === +userId;
+
+  const { data, error, loading } = useData({
+    path: `users/${userId}`,
+    dependencies: [userId],
+  });
+  const { posts, reposts, updatePost, updateRepost } = usePosts({
+    postsPath: `users/${userId}/posts`,
+    fetchReposts: true,
+  });
+  const [updateLikes, setUpdateLikes] = useState(false);
+  const doUpdateLikes = () => setUpdateLikes(!updateLikes);
+  const { data: likesData } = useData({
+    path: `users/${user.id}/interactions?type=LIKE`,
+    dependencies: [updateLikes],
+  });
+
+  return (
+    <S.Profile>
+      {data && <S.Title>{data.user.profile.displayName}</S.Title>}
+      {error ? (
+        <Error>{error}</Error>
+      ) : loading ? (
+        <Loading data="profile" />
+      ) : (
+        <ProfileHeader user={data.user} isUser={isUser} />
+      )}
+      <ProfileContent
+        content={{
+          posts,
+          reposts,
+          likes: likesData ? likesData.interactions : [],
+        }}
+        update={{
+          post: updatePost,
+          repost: updateRepost,
+          likes: doUpdateLikes,
+        }}
+        isUser={isUser}
+      />
+    </S.Profile>
+  );
+}
 
 export default Profile;
---
+++ client/src/pages/dashboard/profile/Profile.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const Profile = styled.main``;
+const Title = styled.header``;
+
+export { Profile, Title };
