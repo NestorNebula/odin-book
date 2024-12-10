a6c01a75225a0b81cc14c523d21a31db33e5d591
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-10 10:32:17 +0100

    feat: add content to Post

--- client/src/pages/dashboard/post/Post.jsx
+++ client/src/pages/dashboard/post/Post.jsx
@@ -1,3 +1,33 @@
-function Post() {}
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useData } from '@hooks';
+import { DefaultSidebar, Error, Loading, PostDetails } from '@components';
+import * as S from './Post.styles';
+
+function Post() {
+  const [update, setUpdate] = useState(false);
+  const doUpdate = () => setUpdate(!update);
+  const { postId } = useParams();
+  const { data, error, loading } = useData({
+    path: `posts/${postId}`,
+    dependencies: [update, postId],
+  });
+
+  return (
+    <S.Post>
+      <S.Header>
+        <div>Post</div>
+      </S.Header>
+      {error ? (
+        <Error>{error}</Error>
+      ) : loading ? (
+        <Loading data="post" />
+      ) : (
+        <PostDetails post={data.post} update={doUpdate} />
+      )}
+      <DefaultSidebar />
+    </S.Post>
+  );
+}
 
 export default Post;
---
+++ client/src/pages/dashboard/post/Post.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+const Post = styled.main``;
+const Header = styled.header``;
+
+export { Post, Header };
