164d16de31eded8088bd98035af668b38676479c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-10 16:36:44 +0100

    feat: add styles for Button

--- client/src/components/elements/Button.jsx
+++ client/src/components/elements/Button.jsx
@@ -1,11 +1,26 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const StyledButton = styled.button``;
+const StyledButton = styled.button`
+  background-color: ${(props) =>
+    props.$backgroundColor ?? props.theme.mainWhite};
+  color: ${(props) => props.$color ?? props.theme.black};
+  border: ${(props) =>
+    props.$backgroundColor === props.theme.black
+      ? `1px solid ${props.theme.secondaryGray}`
+      : null};
+  padding: 0.75rem;
+  border-radius: 25px;
+`;
 
-function Button({ children, onClick }) {
+function Button({ children, onClick, ...props }) {
   return (
-    <StyledButton type="button" onClick={onClick}>
+    <StyledButton
+      type="button"
+      onClick={onClick}
+      $backgroundColor={props.backgroundColor}
+      $color={props.color}
+    >
       {children}
     </StyledButton>
   );
@@ -14,6 +29,8 @@ function Button({ children, onClick }) {
 Button.propTypes = {
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
   onClick: PropTypes.func.isRequired,
+  backgroundColor: PropTypes.string,
+  color: PropTypes.string,
 };
 
 export default Button;
