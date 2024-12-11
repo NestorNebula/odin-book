b5f919009e41542ed9b951a73db87292ed51c68c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-11 10:36:45 +0100

    feat: add check for useId before calling db

--- server/controllers/user/get.js
+++ server/controllers/user/get.js
@@ -2,6 +2,9 @@ const prisma = require('../../models/queries');
 const Sperror = require('sperror');
 
 const getUser = async (req, res, next) => {
+  if (isNaN(+req.params.userId)) {
+    return next(new Sperror('Bad Request', 'Invalid user.', 400));
+  }
   const user = await prisma.findUserById(+req.params.userId);
   if (!user) {
     return next(new Sperror('Not Found', "This user doesn't exist.", 404));
--- server/controllers/user/post/getAll.js
+++ server/controllers/user/post/getAll.js
@@ -2,6 +2,9 @@ const prisma = require('../../../models/queries');
 const Sperror = require('sperror');
 
 const getAllPosts = async (req, res, next) => {
+  if (isNaN(+req.params.userId)) {
+    return next(new Sperror('Bad Request', 'Invalid user.', 400));
+  }
   const user = await prisma.findUserById(+req.params.userId);
   if (!user) {
     return next(new Sperror('Not Found', "User doesn't exist", 404));
