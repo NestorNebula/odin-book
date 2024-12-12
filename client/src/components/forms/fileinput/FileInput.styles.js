9e44cfda17c81f3b1b059a23cefd64910daed11b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-12 11:14:49 +0100

    feat: add styles to FileInput

--- client/src/components/forms/fileinput/FileInput.styles.js
+++ client/src/components/forms/fileinput/FileInput.styles.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
 const Container = styled.div``;
-const Label = styled.label``;
-const Input = styled.input``;
+const Label = styled.label`
+  filter: brightness(0) saturate(100%) invert(61%) sepia(55%) saturate(5111%)
+    hue-rotate(179deg) brightness(97%) contrast(93%);
+  & > img {
+    width: 2.5rem;
+  }
+`;
+const Input = styled.input`
+  display: none;
+`;
 
 export { Container, Label, Input };
