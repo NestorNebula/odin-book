621935cf23321448cb160f7a40e2ae827053541c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-12 12:14:40 +0100

    fix: calls recursive function correctly

--- server/helpers/post-recursion.js
+++ server/helpers/post-recursion.js
@@ -4,7 +4,7 @@ const deletePostCommentsRecursively = async (post) => {
   for (let i = 0; i < post.comments.length; i++) {
     const comment = await prisma.findPost(post.comments[i].id);
     if (comment.comments.length) {
-      await deleteComments(comment);
+      await deletePostCommentsRecursively(comment);
     } else {
       await prisma.deletePost(comment);
     }
