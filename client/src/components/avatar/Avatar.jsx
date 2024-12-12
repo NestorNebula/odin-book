cf62adfa6dc1cd8b0a67f1fa764a03d53494c170
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-12 09:18:46 +0100

    feat: add styles to Avatar

--- client/src/components/avatar/Avatar.jsx
+++ client/src/components/avatar/Avatar.jsx
@@ -2,10 +2,13 @@ import PropTypes from 'prop-types';
 import { avatar } from '@assets/icons';
 import styled from 'styled-components';
 
-const StyledAvatar = styled.img``;
+const StyledAvatar = styled.img`
+  width: ${(props) => props.$width ?? '5rem'};
+  border-radius: ${(props) => props.$width ?? '5rem'};
+`;
 
 function Avatar({ profile }) {
-  return <StyledAvatar src={profile.avatar ?? avatar} />;
+  return <StyledAvatar src={profile.avatar ?? avatar} alt="" />;
 }
 
 Avatar.propTypes = {
