a8aa39033a7f7e0e1e089248a73a0a7a2ee51f19
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-13 15:51:31 +0100

    feat: add styles to FollowButton

--- client/src/components/followbutton/FollowButton.styles.js
+++ client/src/components/followbutton/FollowButton.styles.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components';
 
-const FollowButton = styled.button``;
-const FollowingButton = styled.button``;
-const UnfollowButton = styled.button``;
+const FollowButton = styled.button`
+  background-color: ${(props) => props.theme.mainWhite};
+  color: ${(props) => props.theme.black};
+  border-radius: 2.5rem;
+  font-weight: 700;
+  width: min(100%, 10rem);
+  height: fit-content;
+  padding: 0.75rem 0;
+
+  &:hover {
+    background-color: ${(props) => props.theme.fourthGray};
+  }
+`;
+const FollowingButton = styled(FollowButton)`
+  background-color: ${(props) => props.theme.black};
+  color: ${(props) => props.theme.secondaryWhite};
+  border: ${(props) => `1px solid ${props.theme.secondaryGray}`};
+`;
+const UnfollowButton = styled(FollowButton)`
+  background-color: ${(props) => props.theme.black};
+  color: ${(props) => props.theme.red};
+  border: ${(props) => `1px solid ${props.theme.red}`};
+
+  &:hover {
+    background-color: ${(props) => props.theme.black};
+  }
+`;
 
 export { FollowButton, FollowingButton, UnfollowButton };
