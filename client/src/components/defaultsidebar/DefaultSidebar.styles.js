043e5a3512b589782731254571b04a28099df4c8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-13 15:52:07 +0100

    feat: add styles to DefaultSidebar

--- client/src/components/defaultsidebar/DefaultSidebar.styles.js
+++ client/src/components/defaultsidebar/DefaultSidebar.styles.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
-const Sidebar = styled.aside``;
+const Sidebar = styled.aside`
+  padding: 0.25rem 3rem;
+  width: min(400px, 100%);
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  border-left: ${(props) => `1px solid ${props.theme.fifthGray}`};
+
+  & > div {
+    border: ${(props) => `1px solid ${props.theme.fifthGray}`};
+  }
+`;
 
 export { Sidebar };
