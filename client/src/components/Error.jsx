8b1ac32941db8f21af7bd3c2984e03be1dddfd25
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-16 18:23:55 +0100

    feat: add styles to Error element

--- client/src/components/Error.jsx
+++ client/src/components/Error.jsx
@@ -2,13 +2,36 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const StyledError = styled.div``;
+const StyledError = styled.div`
+  place-self: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  font-size: 2rem;
+  padding: 1rem;
+
+  & > a {
+    color: ${(props) => props.theme.mainBlue};
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+const StyledStatus = styled.div`
+  font-style: italic;
+  color: ${(props) => props.main.secondaryGray};
+`;
+const StyledErrorContent = styled.div`
+  font-size: 2.5rem;
+  color: ${(props) => props.theme.secondaryWhite};
+`;
 
 function Error({ children, status, provideRedirect }) {
   return (
     <StyledError>
-      {status && <div>{status}</div>}
-      <div>{children}</div>
+      {status && <StyledStatus>{status}</StyledStatus>}
+      <StyledErrorContent>{children}</StyledErrorContent>
       {provideRedirect && <Link to="/">Return to Homepage.</Link>}
     </StyledError>
   );
