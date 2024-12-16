77f116bc771153ae194909831a30a3a2ff5132d6
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-16 18:24:18 +0100

    feat: add styles to Loading element

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,7 +1,21 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const StyledLoading = styled.div``;
+const loadingAnimation = keyframes`
+from {
+  opacity: 0;
+}
+to {
+  opacity: 1;
+}
+`;
+
+const StyledLoading = styled.div`
+  place-self: center;
+  font-size: 2rem;
+  padding: 1rem;
+  animation: ${loadingAnimation} 0.5s ease-out alternate infinite;
+`;
 
 function Loading({ data }) {
   return <StyledLoading>Loading {data}...</StyledLoading>;
