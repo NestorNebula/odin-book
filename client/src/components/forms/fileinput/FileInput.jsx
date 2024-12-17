b3a4120fdc78551fbb65a7d90fd490d0a2bcc062
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-17 17:18:43 +0100

    fix: add unique id to file input

--- client/src/components/forms/fileinput/FileInput.jsx
+++ client/src/components/forms/fileinput/FileInput.jsx
@@ -5,14 +5,15 @@ import * as S from './FileInput.styles';
 
 function FileInput({ onChange, image }) {
   const { user } = useContext(Context);
+  const id = window.crypto.randomUUID();
 
   return (
     <S.Container $hidden={user.loginMethod === 'GUEST'}>
-      <S.Label htmlFor="file">
+      <S.Label htmlFor={`file${id}`}>
         <img src={image} alt="upload image" />
       </S.Label>
       <S.Input
-        id="file"
+        id={`file${id}`}
         name="file"
         type="file"
         value={''}
