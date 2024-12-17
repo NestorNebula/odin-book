d12b7a29a7e7884bc883f737cea55de429bd39ea
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-12-17 10:50:58 +0100

    feat: set Loading back to true on search update

--- client/src/hooks/useSearchData.jsx
+++ client/src/hooks/useSearchData.jsx
@@ -14,6 +14,7 @@ const useSearchData = (search) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     if (!search) {
       setPosts([]);
       setUsers([]);
